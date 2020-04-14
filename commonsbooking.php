0d2eb824a04a65da2f24219e39c3d8d658917f80
Markus Koch <markus.koch@kernpunkt.de>
2020-04-14 13:08:36 +0200

    initial commit

---
+++ commonsbooking.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name:  Commons Booking
+ * Plugin URI: ~
+ * Description: ~
+ * Version: 1.0
+ * License: GPL2
+ * License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:  commonsbooking
+ */
