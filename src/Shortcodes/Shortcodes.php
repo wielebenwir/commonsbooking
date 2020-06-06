3858f11f0b443ac04079987a921cdc772f06b5e1
christian-wlw <github@farbschein.de>
2020-06-06 09:56:24 +0200

    added more shortcode options

--- src/Shortcodes/Shortcodes.php
+++ src/Shortcodes/Shortcodes.php
@@ -1,34 +1,31 @@
 <?php
 
 namespace CommonsBooking\Shortcodes;
+
 use CMB2;
 
 class Shortcodes
 {
     public function __construct()
     {
-        
-        
+
+
         /**
          * Adding all shortcodes 
          * shortcodes can be used in mail-templates or frontend-templates via shortcode-tag [shortcode]
          */
-        add_shortcode('cmb-form', array($this, 'cmb2_do_frontend_form_shortcode' ));
-        add_shortcode('cb_location_pickupinstructions', array($this, 'Shortcode_cb_location_pickupinstructions' ));
-    }
-    
-    
-    
-    /**
-     * TODO: 
-     * - Funktion, die automatisch Shortcodes für alle post-meta-felder anlegt
-     * - Funktion die 
-     * 
-     */
 
-    
+
+        // CMB2 method to register shortcodes for all cmb2 metaboxes
+        add_shortcode('cb-field', array($this, 'postmeta_shortcode'));
+
+        // Register cb-booking shortcode 
+        add_shortcode('cb-booking', array($this, 'metabox_booking_shortcodes'));
+    }
+
     /**
-     * Render all shortcote in a given string, replaces shortcodes with shortcode-value and returns the replaced shortcodes
+     * Render all shortcodes in a given string, replaces shortcodes with shortcode-value and returns the replaced shortcodes
+     * is used in handling email templates
      *
      * @param  mixed $string
      * @return void
@@ -36,19 +33,72 @@ class Shortcodes
     public static function getRenderedShortcodes($content)
     {
         global $shortcode_tags;
-        
-        if (preg_match_all('/' . get_shortcode_regex() . '/s', $content, $matches))
-        {
-            var_dump($matches[2]);
+        print "<pre>";
+        var_dump($shortcode_tags);
+
+        if (preg_match_all('/' . get_shortcode_regex() . '/s', $content, $matches)) {
             foreach ($matches[2] as $i => $sc) {
                 $shortcode = $matches[0][$i];
-                var_dump($shortcode);
                 $replace = do_shortcode($shortcode);
                 $content = str_replace($shortcode, $replace, $content);
-                var_dump($content);
             }
         }
         return $content;
+    }
+
+
+    /**
+     * handles postmeta shortcodes by getting location and item post_meta in context of a given timeframe_id
+     * usage [booking type=item/location id=metabox_id post_id = cb_timeframe_id]
+     *
+     * @param  mixed $atts
+     * @return void
+     */
+    public static function metabox_booking_shortcodes($atts = array())
+    {
+        global $post;
+
+        // If no metabox field is set, yell about it
+        if (empty($atts['field'])) {
+            return __("Please add an 'id' attribute to specify the CMB2 form to display.", CB_TEXTDOMAIN);
+        }
+
+        // check if type is set and get meta and post data from item and location linked to the given timeframe-id
+        if (isset($atts['type']) && ($atts['type'] == "location" || $atts['type'] == "item")) {
+            $timeframe = get_post($_GET['cb_timeframe']);
+            $atts['post_id'] = $timeframe->ID;
+
+            $post_meta = get_post_meta($timeframe->ID, $atts['type'] . '-id');
+            $atts['post_id'] = $post_meta[0];
+            $post = get_post($post_meta[0]);
+        } else {
+            return __("Please add a 'type' attribute, should be either location or item", CB_TEXTDOMAIN);
+        }
+
+        /**
+         * handle special field to get standard post attributes
+         */
+        if ($atts['field'] == "title") {
+            $result = $post->post_title;
+        } else {
+
+            /**
+             * Make sure a WordPress post ID is set.
+             * We'll default to the current post/page
+             */
+            if (!isset($atts['post_id'])) {
+                $atts['post_id'] = $post->ID;
+            }
+
+            $object_id = absint($atts['post_id']);
+            $metabox_id = esc_attr($atts['field']);
+
+            // Get our form
+            //$form =  cmb2_get_metabox_form( $metabox_id, $object_id );
+            $result = get_post_meta($object_id, $metabox_id, true);
+        }
+
+        return $result;
     }
 
 
@@ -57,43 +107,36 @@ class Shortcodes
      * @param  array  $atts Shortcode attributes
      * @return string       Form HTML markup
      */
-    public static function cmb2_do_frontend_form_shortcode( $atts = array() ) {
+    function postmeta_shortcode($atts = array())
+    {
         global $post;
 
         /**
          * Depending on your setup, check if the user has permissions to edit_posts
          */
-        //if ( ! current_user_can( 'edit_posts' ) ) {
-        //    return __( 'You do not have permissions to edit this post.', 'lang_domain' );
-       // }
+        if (!current_user_can('edit_posts')) {
+            return __('You do not have permissions to edit this post.', 'lang_domain');
+        }
 
         /**
          * Make sure a WordPress post ID is set.
          * We'll default to the current post/page
          */
-        if ( ! isset( $atts['post_id'] ) ) {
+        if (!isset($atts['post_id'])) {
             $atts['post_id'] = $post->ID;
         }
 
         // If no metabox id is set, yell about it
-        if ( empty( $atts['id'] ) ) {
-            return __( "Please add an 'id' attribute to specify the CMB2 form to display.", 'lang_domain' );
+        if (empty($atts['field'])) {
+            return __("Please add an 'id' attribute to specify the CMB2 form to display.", 'lang_domain');
         }
 
-        $metabox_id = esc_attr( $atts['id'] );
-        $object_id = absint( $atts['post_id'] );
+        $metabox_id = esc_attr($atts['field']);
+        $object_id = absint($atts['post_id']);
         // Get our form
-        //$form = \cmb2_get_metabox_form( $metabox_id, $object_id );
+        // $form = cmb2_get_metabox_form( $metabox_id, $object_id );
         $form = get_post_meta($object_id, $metabox_id, true);
 
         return $form;
-    }
-
-    public static function Shortcode_cb_location_pickupinstructions() {
-        $timeframe = get_post($_GET['cb_timeframe']);
-        $locationID = get_post_meta($timeframe->ID,'location-id');
-        $postmeta = get_post_meta($locationID[0], '_cb_location_pickupinstructions');  
-
-        return $postmeta[0];
     }
 }
