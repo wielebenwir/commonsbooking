306818b3e99afddb47a4646a28310b17e2d4eece
christian-wlw <github@farbschein.de>
2020-06-25 10:10:47 +0200

    minor modifications

--- src/Model/Location.php
+++ src/Model/Location.php
@@ -13,9 +13,9 @@ class Location extends CustomPost
         
         $location = array (
             get_the_title($this->post->ID),
-            get_post_meta($this->post->ID, CB_METABOX_PREFIX . 'location_street', true),
-            get_post_meta($this->post->ID, CB_METABOX_PREFIX . 'location_postcode', true),
-            get_post_meta($this->post->ID, CB_METABOX_PREFIX . 'location_city', true),
+            CB::get('location', CB_METABOX_PREFIX . 'location_street'),
+            CB::get('location', CB_METABOX_PREFIX . 'location_postcode'),
+            CB::get('location', CB_METABOX_PREFIX . 'location_city'),
         );
 
         return implode('<br>', $location);
@@ -24,10 +24,10 @@ class Location extends CustomPost
 
     public function location_contact()
     {   
-        if (!empty(get_post_meta($this->post->ID, CB_METABOX_PREFIX . 'location_contact', true))) {
+        if ( !empty( CB::get( 'location', CB_METABOX_PREFIX . 'location_contact') ) ) {
             $contact[] = "<br>"; // needed for email template
             $contact[] = __( 'Please contact the contact persons at the location directly if you have any questions regarding collection or return:', CB_TEXTDOMAIN );
-            $contact[] = nl2br(get_post_meta($this->post->ID, CB_METABOX_PREFIX . 'location_contact', true));
+            $contact[] = nl2br(CB::get('location',  CB_METABOX_PREFIX . 'location_contact'));
         }
 
         return implode('<br>', $contact);
