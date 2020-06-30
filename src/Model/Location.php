12d4665629f575efe4ba985dfcf4fbccc6d82bdf
christian-wlw <github@farbschein.de>
2020-06-30 18:18:36 +0200

    try to recove lost changes due to messed up git

--- src/Model/Location.php
+++ src/Model/Location.php
@@ -20,18 +20,36 @@ class Location extends CustomPost
     /**
      * Returns location infos.
      * @return string
+     */    
+    /**
+     * location_address
+     *
+     * @return void
      */
-    public function location_information()
+    public function location_address()
     {
         
         $location = array (
-            get_the_title($this->post->ID),
-            CB::get('location', CB_METABOX_PREFIX . 'location_street'),
-            CB::get('location', CB_METABOX_PREFIX . 'location_postcode'),
-            CB::get('location', CB_METABOX_PREFIX . 'location_city'),
+            CB::get('location', CB_METABOX_PREFIX . 'location_street', $this->post->ID),
+            CB::get('location', CB_METABOX_PREFIX . 'location_postcode', $this->post->ID),
+            CB::get('location', CB_METABOX_PREFIX . 'location_city', $this->post->ID),
         );
 
         return implode('<br>', $location);
+
+    }
+    /**
+     * Return Address @TODO Formatting 
+     * @return string
+     */
+    public function address()
+    {
+        return sprintf(
+            '%s, %s %s',
+            CB::get('location', CB_METABOX_PREFIX . 'location_street'),
+            CB::get('location', CB_METABOX_PREFIX . 'location_postcode'),
+            CB::get('location', CB_METABOX_PREFIX . 'location_city')
+        );
 
     }
 
@@ -43,7 +61,7 @@ class Location extends CustomPost
     {   
         if ( !empty( CB::get( 'location', CB_METABOX_PREFIX . 'location_contact') ) ) {
             $contact[] = "<br>"; // needed for email template
-            $contact[] = __( 'Please contact the contact persons at the location directly if you have any questions regarding collection or return:', CB_TEXTDOMAIN );
+            $contact[] = __( 'Please contact the contact persons at the location directly if you have any questions regarding collection or return:', 'commonsbooking' );
             $contact[] = nl2br(CB::get('location',  CB_METABOX_PREFIX . 'location_contact'));
         }
 
