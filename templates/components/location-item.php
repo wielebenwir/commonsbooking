a581a42c6adf365d2f2604aeeffcba58a41760f9
christian-wlw <github@farbschein.de>
2020-07-01 15:07:51 +0200

    template modifications via flo / due to github issue

--- templates/components/location-item.php
+++ templates/components/location-item.php
@@ -1,10 +1,15 @@
-<div>
-    <h3>
-        <a href="<?php echo get_permalink( $templateData['item']->ID); ?>"><?php echo $templateData['item']->post_title; ?></a></h3>
-        <?php echo get_the_post_thumbnail( $templateData['item']->ID, 'thumbnail' ); ?>
-    <br>
-</div>
+<div class="cb-box">
+    <div class="cb-cols col-30-70">
+        <div>
+            <?php echo get_the_post_thumbnail( $templateData['item']->ID, 'thumbnail' ); ?>
+        </div>
+        <div>
+        <h3>
+            <a href="<?php echo get_permalink( $templateData['item']->ID); ?>"><?php echo $templateData['item']->post_title; ?></a></h3>
+        </div>
+    </div>
 
 <?php
     include __DIR__ . '/calendar-index.php';
 ?>
+</div>
