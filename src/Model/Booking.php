40742d7926133aa04aef6046d9818fddda401e0b
christian-wlw <github@farbschein.de>
2020-07-02 23:43:33 +0200

    modified date / format to deal with language settings

--- src/Model/Booking.php
+++ src/Model/Booking.php
@@ -115,14 +115,12 @@ class Booking extends CustomPost
         $date_format = get_option('date_format');
         $time_format = get_option('time_format');
         
-        $date = date($date_format, $this->get_meta($date_type .'-date'));
-        $time_start = date($time_format, $this->get_meta($date_type . '-date'));
+        $date = date_i18n($date_format, $this->get_meta($date_type .'-date'));
+        $time_start = date_i18n($time_format, $this->get_meta($date_type . '-date'));
 
         $grid = $this->get_meta('grid');
         $full_day = $this->get_meta('full-day');
 
-        var_dump($grid);
-        var_dump($full_day);
 
         if ($full_day == "on") {
             return $date;
@@ -153,8 +151,8 @@ class Booking extends CustomPost
         $date_format = get_option('date_format');
         $time_format = get_option('time_format');
         
-        $date_start = date($date_format, $this->get_meta('start-date'));
-        $time_start = date($time_format, $this->get_meta('start-date'));
+        $date_start = date_i18n($date_format, $this->get_meta('start-date'));
+        $time_start = date_i18n($time_format, $this->get_meta('start-date'));
 
         $grid = $this->get_meta('grid');
         $full_day = $this->get_meta('full-day');
@@ -164,11 +162,11 @@ class Booking extends CustomPost
         }
 
         if ($grid > 0) { // if bookable grid is set to hour
-            $time_end = date($time_format, $this->get_meta('start-date') + (60 * 60 * $grid));
+            $time_end = date_i18n($time_format, $this->get_meta('start-date') + (60 * 60 * $grid));
         }
 
         if ($grid == 0) { // if grid is set to slot duration
-            $time_end = date($time_format, strtotime($this->get_meta('end-time')));
+            $time_end = date_i18n($time_format, strtotime($this->get_meta('end-time')));
         }
 
         return $date_start . ' ' . $time_start . ' - ' . $time_end;;
@@ -184,8 +182,8 @@ class Booking extends CustomPost
         $date_format = get_option('date_format');
         $time_format = get_option('time_format');
         
-        $date_end = date($date_format, $this->get_meta('end-date'));
-        $time_end = date($time_format, $this->get_meta('end-date') + 60 ); // we add 60 seconds because internal timestamp is set to hh:59 
+        $date_end = date_i18n($date_format, $this->get_meta('end-date'));
+        $time_end = date_i18n($time_format, $this->get_meta('end-date') + 60 ); // we add 60 seconds because internal timestamp is set to hh:59 
 
         $grid = $this->get_meta('grid');
         $full_day = $this->get_meta('full-day');
@@ -195,11 +193,11 @@ class Booking extends CustomPost
         }
 
         if ($grid > 0) {
-            $time_start = date($time_format, $this->get_meta('end-date') +1 -(60 * 60 * $grid) );
+            $time_start = date_i18n($time_format, $this->get_meta('end-date') +1 -(60 * 60 * $grid) );
         }
 
         if ($grid == 0) { // if grid is set to slot duration
-            $time_start = date($time_format, strtotime($this->get_meta('start-time')));
+            $time_start = date_i18n($time_format, strtotime($this->get_meta('start-time')));
         }
 
         return $date_end . ' ' . $time_start . ' - ' . $time_end;;
