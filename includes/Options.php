c5dfd0a3fcf04d52c930fbb403164971b400d8a7
christian-wlw <github@farbschein.de>
2020-07-05 11:02:01 +0200

    update translation and default text

--- includes/Options.php
+++ includes/Options.php
@@ -194,7 +194,10 @@ $options_array = array(
             </p>
             
             
-            <p>Click here to see or cancel your booking: {{booking-permalink}}.</p>
+            <p>Click here to see or cancel your booking: {{booking:booking_link}}.</p><br>
+            <b>Notice:</b> You need to be logged in to see your booking. <br>
+            If the link leads you to the homepage of the webseite,
+            please login first and then click the link again.<br><br>
             
             <h3>Your information</h3>
             <p>Login: {{user:user_nicename}}<br>
