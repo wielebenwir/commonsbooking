63296d44a823331058b960465794098685857e67
Markus Koch <markus@mach-websites.de>
2020-11-18 23:06:09 +0100

    disable booking button if no end date is selected, changed date selection function to new onDaySelect

--- templates/timeframe-calendar.php
+++ templates/timeframe-calendar.php
@@ -38,7 +38,7 @@
             </div>
             <?php
             if(is_user_logged_in()) { ?>
-                <input type="submit" value="<?php echo __('Continue to check booking', 'commonsbooking'); ?>" />
+                <input type="submit" disabled="disabled" value="<?php echo __('Continue to check booking', 'commonsbooking'); ?>" />
             <?php } ?>
         </form>
     </div>
