4e98f67c9801f18023e2457aff73644ae09e65d6
Florian Egermann <florian@macht-medien.de>
2020-11-30 21:37:41 +0000

    Added license and restored zipdist fucntionality

--- zipdist.sh
+++ zipdist.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-git archive -o commonsbooking.zip HEAD
+version=$(awk '/[^[:graph:]]Version/{print $NF}' commonsbooking.php)
+echo "Zipping Version $version..."
+zip -r "commonsbooking-$version.zip" assets includes languages src templates vendor commonsbooking.php index.php LICENSE.txt readme.txt
