b669058e6b321be39d54a01554df572fe5795568
christian-wlw <github@farbschein.de>
2020-12-17 00:25:01 +0100

    fix minor santiziation issue

--- src/View/BookingCodes.php
+++ src/View/BookingCodes.php
@@ -25,7 +25,7 @@ class BookingCodes
                     <div class="cmb-td">
                         <table>
                             <tr>
-                                <td><b>'.esc_html_e('Item', 'commonsbooking').'</b></td><td><b>' . esc_html_e('Pickup date', 'commonsbooking').'</b></td><td><b>'.esc_html_e('Code', 'commonsbooking').'</b></td>
+                                <td><b>'.esc_html__('Item', 'commonsbooking').'</b></td><td><b>' . esc_html__('Pickup date', 'commonsbooking').'</b></td><td><b>'.esc_html__('Code', 'commonsbooking').'</b></td>
                             </tr>';
 
         /** @var BookingCode $bookingCode */
