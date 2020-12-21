20287f620114337b15857fb1adca45bb45040fbf
Markus Koch <markus@mach-websites.de>
2020-12-21 08:33:25 +0100

    changed wording for functions, changed optionsArray include

--- src/Wordpress/Options/AdminOptions.php
+++ src/Wordpress/Options/AdminOptions.php
@@ -6,27 +6,27 @@ use CommonsBooking\Settings\Settings;
 class AdminOptions
 {
     private static $option_key = COMMONSBOOKING_PLUGIN_SLUG . '_options';
-    
+
     /**
      * set default values to admin options fields as defined in includes/OptionsArray.php
      *
      * @return void
      */
-    public static function SetOptionsDefaultValues() {
+    public static function setOptionsDefaultValues() {
 
-        include(COMMONSBOOKING_PLUGIN_DIR . '/includes/OptionsArray.php');        
+        $options_array = include(COMMONSBOOKING_PLUGIN_DIR . '/includes/OptionsArray.php');
         foreach ($options_array as $tab_id => $tab) {
 
             $groups = $tab['field_groups'];
-            
+
             foreach ($groups as $group_id => $group) {
 
                 $fields = $group['fields'];
                 $option_key = self::$option_key . '_' . $tab_id;
                 $option = array();
-                
+
                 foreach ($fields as $field) {
-                    
+
                     // we check if there there is a default value for this field
                     if (array_key_exists( 'default', $field ) ) {
                         // if field-value is not set already we add the default value to the options array
@@ -35,8 +35,8 @@ class AdminOptions
                         }
                     }
                 }
-    
-                // update option 
+
+                // update option
                 if (!empty ( $option ) ) {
                     update_option($option_key, $option);
                 }
