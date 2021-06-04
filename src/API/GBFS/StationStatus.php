74d45a8e8a5db223e675125874c912e18c851911
Markus Koch <markus@mach-websites.de>
2021-06-04 11:51:36 +0200

    added routes for gbfs item-status and item-information

---
+++ src/API/GBFS/StationStatus.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace CommonsBooking\API\GBFS;
+
+
+use CommonsBooking\Model\Location;
+use CommonsBooking\Repository\Item;
+
+class StationStatus extends BaseRoute
+{
+
+    /**
+     * The base of this controller's route.
+     *
+     * @var string
+     */
+    protected $rest_base = 'station_status';
+
+    /**
+     * Commons-API schema definition.
+     * @var string
+     */
+    protected $schemaUrl = "https://raw.githubusercontent.com/MobilityData/gbfs-json-schema/master/station_status.json";
+
+    /**
+     * @param $item Location
+     * @param $request
+     *
+     * @return \stdClass
+     * @throws \Geocoder\Exception\Exception
+     */
+    public function prepare_item_for_response($item, $request)
+    {
+        $preparedItem = new \stdClass();
+        $preparedItem->station_id = $item->ID . "";
+        $preparedItem->num_bikes_available = count(Item::getByLocation($item->ID));
+        $preparedItem->is_installed = true;
+        $preparedItem->is_renting = true;
+        $preparedItem->is_returning = true;
+
+        return $preparedItem;
+    }
+
+}
