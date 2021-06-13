d95ccc622df73d66a621523c4f0dc66673c5a4c4
christian-wlw <github@farbschein.de>
2021-06-13 11:06:03 +0200

    update location template to hide contact details on location single page

--- templates/location-single-meta.php
+++ templates/location-single-meta.php
@@ -13,6 +13,10 @@ $location = $templateData['location'];
 $location_address   =  $location->formattedAddressOneLine();
 $location_contact   =  $location->formattedContactInfoOneLine();
 $pickup_instructions = $location->formattedPickupInstructions();
+$show_contactinfo_unconfirmed = \CommonsBooking\Settings\Settings::getOption('commonsbooking_options_templates', 'show_contactinfo_unconfirmed');
+$text_hidden_contactinfo = \CommonsBooking\Settings\Settings::getOption('commonsbooking_options_templates', 'text_hidden-contactinfo');
+
+
 ?>
 
 <div class="cb-list-content cb-location-address cb-col-30-70">
@@ -23,7 +27,19 @@ $pickup_instructions = $location->formattedPickupInstructions();
 <?php if ( $location_contact ) { ?>
 <div class="cb-list-content cb-location-contact cb-col-30-70">
   <div><?php echo esc_html__('Location contact', 'commonsbooking'); ?></div>
-  <div><?php echo $location_contact; ?></div>
+  <?php 
+    // show contact details only after booking if options are set to show contactinfo even on unconfirmed booking status
+    if( $show_contactinfo_unconfirmed == 'on') { ?>
+        <div><?php echo $location->formattedContactInfoOneLine(); ?></div>
+<?php
+// else; show info-text to inform user to confirm booking to see contact details
+} else {
+?>
+        <div><strong><?php echo $text_hidden_contactinfo; ?></strong></div>
+<?php 
+// end if booking == confirmed
+}
+?>
 </div> <!-- .cb-cb-contact -->
 <?php } // if ( $location_contact ) ?>
 
