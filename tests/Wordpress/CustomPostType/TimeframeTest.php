8952162d5eb2e45143ab1d4ef5e0be72d2b10e75
Markus Koch <markus@mach-websites.de>
2021-07-01 18:04:29 +0200

    added first small test

---
+++ tests/Wordpress/CustomPostType/TimeframeTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Wordpress\CustomPostType;
+
+use CommonsBooking\Wordpress\CustomPostType\Timeframe;
+use PHPUnit\Framework\TestCase;
+
+class TimeframeTest extends TestCase {
+
+	public $testPostId;
+
+	public function testIsLocked() {
+		$timeframe = get_post( $this->testPostId );
+		$this->assertTrue( Timeframe::isLocked( $timeframe ) );
+	}
+
+	public function testIsOverBookable() {
+		$timeframe = get_post( $this->testPostId );
+		$this->assertFalse( Timeframe::isOverBookable( $timeframe ) );
+	}
+
+	protected function setUp(): void {
+		$this->testPostId = wp_insert_post( [
+			'post_title'   => 'Sample Post',
+			'post_content' => 'This is just some sample post content.'
+		] );
+
+		// Timeframe is a booking
+		update_post_meta( $this->testPostId, 'type', Timeframe::BOOKING_ID );
+	}
+
+	protected function tearDown(): void {
+		wp_delete_post( $this->testPostId, true );
+	}
+}
