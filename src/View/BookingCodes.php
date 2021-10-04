d7a1ba8f8596fcb5334266f93aa0a94c4ae09e02
Markus Koch <markus@mach-websites.de>
2021-10-04 14:47:13 +0200

    fixed issue in booking code visibility

--- src/View/BookingCodes.php
+++ src/View/BookingCodes.php
@@ -19,28 +19,33 @@ class BookingCodes {
 		echo '
             <div class="cmb-row cmb2-id-booking-codes-info">
                 <div class="cmb-th">
-                    <label for="booking-codes-list">' . commonsbooking_sanitizeHTML( __( 'Download Booking Codes', 'commonsbooking' ) ) . '</label>
+                    <label for="booking-codes-download">' . commonsbooking_sanitizeHTML( __( 'Download Booking Codes', 'commonsbooking' ) ) . '</label>
                 </div>
                 <div class="cmb-td">
-                    <a id="booking-codes-list" href="' . esc_url( admin_url( 'post.php' ) ) . '?post=' . $timeframeId . '&action=csvexport" target="_blank"><strong>Download Booking Codes</strong></a></br>
+                    <a id="booking-codes-download" href="' . esc_url( admin_url( 'post.php' ) ) . '?post=' . $timeframeId . '&action=csvexport" target="_blank"><strong>Download Booking Codes</strong></a></br>
                     ' . commonsbooking_sanitizeHTML( __( 'The file will be exported as tab delimited .txt file so you can choose wether you want to print it, open it in a separate application (like Word, Excel etc.)', 'commonsbooking' ) ) . '
                 </div>
             </div>
-                <div <div class="cmb-row cmb2-id-booking-codes-list">
-                        <table>
-                            <tr>
-                                <td><b>' . esc_html__( 'Item', 'commonsbooking' ) . '</b></td><td><b>' . esc_html__( 'Pickup date', 'commonsbooking' ) . '</b></td><td><b>' . esc_html__( 'Code', 'commonsbooking' ) . '</b></td>
-                            </tr>';
+            <div class="cmb-row cmb2-id-booking-codes-list">
+            	<div class="cmb-th">
+                    <label for="booking-codes-list">' . commonsbooking_sanitizeHTML( __( 'Booking codes list', 'commonsbooking' ) ) . '</label>
+                </div>
+	            <div class="cmb-td">
+	                <table id="booking-codes-list">
+	                    <tr>
+	                        <td><b>' . esc_html__( 'Item', 'commonsbooking' ) . '</b></td><td><b>' . esc_html__( 'Pickup date', 'commonsbooking' ) . '</b></td><td><b>' . esc_html__( 'Code', 'commonsbooking' ) . '</b></td>
+	                    </tr>';
 
-		/** @var BookingCode $bookingCode */
-		foreach ( $bookingCodes as $bookingCode ) {
-			echo "<tr>
-                    <td>" . $bookingCode->getItemName() . "</td>
-                    <td>" . $bookingCode->getDate() . "</td>
-                    <td>" . $bookingCode->getCode() . "</td>
-                </tr>";
-		}
+						/** @var BookingCode $bookingCode */
+						foreach ( $bookingCodes as $bookingCode ) {
+							echo "<tr>
+				                    <td>" . $bookingCode->getItemName() . "</td>
+				                    <td>" . $bookingCode->getDate() . "</td>
+				                    <td>" . $bookingCode->getCode() . "</td>
+				                </tr>";
+						}
 		echo '    </table>
+	            </div>
             </div>';
 	}
 
