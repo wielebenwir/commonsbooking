596290d8de39c05b7a6fb089e71715732c608439
christian-wlw <github@farbschein.de>
2021-12-16 14:07:15 +0100

    update timeframe booking code description

--- src/Wordpress/CustomPostType/Timeframe.php
+++ src/Wordpress/CustomPostType/Timeframe.php
@@ -634,7 +634,11 @@ class Timeframe extends CustomPostType {
 			),
 			array(
 				'name' => esc_html__( "Booking Codes", 'commonsbooking' ),
-				'desc' => esc_html__( 'The following things you need to know about booking codes... ', 'commonsbooking' ),
+				'desc' => commonsbooking_sanitizeHTML( __( 'You can automatically generate booking codes. Codes can be generated only with the following settings:</br>
+				- Whole day is enabled</br>
+				- Start date and end date are set</br>
+				<a href="https://commonsbooking.org/?p=437" target="_blank">More Information in the documentation</a>
+				', 'commonsbooking' ) ),
 				'id'   => "title-timeframe-booking-codes",
 				'type' => 'title',
 			),
