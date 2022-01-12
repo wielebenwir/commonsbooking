e60d8e00489784ee9d87e91243fde63403384620
HansMorb <6433480+hansmorb@users.noreply.github.com>
2022-01-12 23:13:45 +0100

    added location link in calendar header

--- templates/location-calendar-header.php
+++ templates/location-calendar-header.php
@@ -4,7 +4,7 @@
     echo $location->thumbnail(array ('200')); // div.thumbnail is printed by function
 ?>
 <div class="cb-list-info">
-    <h4 class="cb-title cb-location-title"><?php echo $location->post_title; ?></h4>
+    <h4 class="cb-title cb-location-title"><a href="<?php echo get_the_permalink($location->ID);?>"> <?php echo $location->post_title; ?></a></h4>
     <div class="cb-address cb-location-address"><?php echo $location->formattedAddressOneLine(); ?></div>
 	<?php
 	    if ( $location->hasMap() ) {
