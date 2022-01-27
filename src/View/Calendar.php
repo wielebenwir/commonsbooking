02ecb30f55542aa74fcf12909afdc547b6a77e84
Markus Koch <markus@mach-websites.de>
2022-01-27 20:06:51 +0100

    fixed bug, that not nearest timeframe was found

--- src/View/Calendar.php
+++ src/View/Calendar.php
@@ -359,7 +359,7 @@ class Calendar {
 	private static function getClosestBookableTimeFrameForToday($bookableTimeframes): ?\CommonsBooking\Model\Timeframe {
 		// Sort timeframes by startdate
 		usort( $bookableTimeframes, function ( $item1, $item2 ) {
-			return abs(time() - $item1->getStartDate()) <=> abs(time() - $item2->getStartDate());
+			return abs(time() - $item1->getStartDate()) < abs(time() - $item2->getStartDate());
 		} );
 
 		return array_pop( $bookableTimeframes );
