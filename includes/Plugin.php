b24f8edfbdc9ddaa9432a54d557e10e75063cea9
christian-wlw <github@farbschein.de>
2022-02-14 23:36:55 +0100

    removed debug logging

--- includes/Plugin.php
+++ includes/Plugin.php
@@ -21,6 +21,7 @@ register_deactivation_hook(
 
 /**
  * writes messages to error_log file
+ * only active if DEBUG_LOG is on
  *
  * @param mixed $log can be a string, array or object
  * @param bool $backtrace if set true the file-path and line of the calling file will be added to the error message
@@ -28,6 +29,10 @@ register_deactivation_hook(
  * @return void
  */
 function commonsbooking_write_log( $log, $backtrace = true ) {
+
+    if (WP_DEBUG_LOG != true ) {
+        return;
+    }
 
     if ( is_array( $log ) || is_object( $log ) ) {
 		$logmessage = ( print_r( $log, true ) );
