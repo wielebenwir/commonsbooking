f3730c2d5f32958f7c33dd6c949b4eafb18f08c6
HansMorb <6433480+hansmorb@users.noreply.github.com>
2022-04-01 21:20:26 +0200

    gets localized timestamp fixes #985

--- src/Model/Booking.php
+++ src/Model/Booking.php
@@ -49,7 +49,7 @@ class Booking extends \CommonsBooking\Model\Timeframe {
 		);
 		$wpdb->query( $sql );
 
-		add_post_meta( $this->post->ID, 'cancellation_time', time() );
+		add_post_meta( $this->post->ID, 'cancellation_time', current_time('timestamp') );
 
 		$this->sendCancellationMail();
 	}
