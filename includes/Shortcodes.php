dd0e06aadf747b16b854f2de7348d9542bfd43f6
HansMorb <6433480+hansmorb@users.noreply.github.com>
2022-04-09 20:45:30 +0200

    removes tag shortcode, fixes typo

--- includes/Shortcodes.php
+++ includes/Shortcodes.php
@@ -1,16 +1,5 @@
 <?php
 
-function commonsbooking_tag( $atts ) {
-	$atts = shortcode_atts( array(
-		'tag' => '',
-	), $atts, 'cb' );
-
-	echo commonsbooking_sanitizeHTML( commonsbooking_parse_shortcode( $atts['tag'] ) );
-}
-
-add_shortcode( 'cb', 'commonsbooking_tag' );
-
-
 //adds shortcode for user statistics
 add_shortcode( 'cb_statistics-user' , array( \CommonsBooking\View\Statistics::class, 'shortcodeUser' ) );
 
