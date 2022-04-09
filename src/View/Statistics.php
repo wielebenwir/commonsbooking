5130408626e37d18f8e01efbb8fe1b8e95b1e28d
farbschein <github@farbschein.de>
2022-04-09 21:11:18 +0200

    WIP first draft stat function

--- src/View/Statistics.php
+++ src/View/Statistics.php
@@ -2,9 +2,23 @@
 
 
 namespace CommonsBooking\View;
-
+use CommonsBooking\Repository\Booking;
+use CommonsBooking\Model\Statistics\User;
 
 class Statistics extends View {
+
+	
+	/**
+	 * __construct
+	 *
+	 * @return void
+	 */
+	public function __construct($startDate) {
+
+		$this->starDate = $startDate;
+
+		$this->bookings = Booking::getForCurrentUser( 'true', $startDate );
+	}
 
 	/**
 	 * Callback function for user statistics
@@ -22,6 +36,29 @@ class Statistics extends View {
 		commonsbooking_get_template_part( 'shortcode', 'statistics-user', true, false, false );
 
 		return ob_get_clean();
+	}
+
+
+	/**
+	 * Returns array with user based statistics
+	 *
+	 * @param  mixed $startDate
+	 * @param  mixed $endDate
+	 */
+	public function getTotalBookingsCountforUser() {
+		$data = array();
+
+		foreach ($this->bookings as $booking) {
+
+			if (!array_key_exists( $booking->post_author, $data )) {
+				$data[$booking->post_author] = new User();	
+			} 
+			
+			$data[$booking->post_author]->addBooking($booking);		
+		}
+
+		return $data;
+
 	}
 
 }
