7111992236323eecce043785fdec7091411d9dd5
HansMorb <6433480+hansmorb@users.noreply.github.com>
2022-04-16 23:00:57 +0200

    added ical creation to settings

--- assets/admin/js/admin.js
+++ assets/admin/js/admin.js
@@ -60,6 +60,40 @@
 (function($) {
     "use strict";
     $(function() {
+        const hideFieldset = function(set) {
+            $.each(set, function() {
+                $(this).parents(".cmb-row").hide();
+            });
+        };
+        const showFieldset = function(set) {
+            $.each(set, function() {
+                $(this).parents(".cmb-row").show();
+            });
+        };
+        const emailform = $("#templates");
+        if (emailform.length) {
+            const eventCreateCheckbox = $("#emailtemplates_mail-booking_ics_attach");
+            const eventTitleInput = $("#emailtemplates_mail-booking_ics_event-title");
+            const eventDescInput = $("#emailtemplates_mail-booking_ics_event-description");
+            const eventFieldSet = [ eventTitleInput, eventDescInput ];
+            const handleiCalAttachmentSelection = function() {
+                showFieldset(eventFieldSet);
+                if (!eventCreateCheckbox.prop("checked")) {
+                    hideFieldset(eventFieldSet);
+                    eventCreateCheckbox.prop("checked", false);
+                }
+            };
+            handleiCalAttachmentSelection();
+            eventCreateCheckbox.click(function() {
+                handleiCalAttachmentSelection();
+            });
+        }
+    });
+})(jQuery);
+
+(function($) {
+    "use strict";
+    $(function() {
         const arrayDiff = function(array1, array2) {
             var newItems = [];
             jQuery.grep(array2, function(i) {
