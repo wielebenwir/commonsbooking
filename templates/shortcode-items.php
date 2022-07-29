a0bc5147df621c6c5a4ddea683d12a457a528024
HansMorb <6433480+hansmorb@users.noreply.github.com>
2022-07-29 17:56:24 +0200

    removed unnecessary variables

--- templates/shortcode-items.php
+++ templates/shortcode-items.php
@@ -15,12 +15,9 @@
 global $templateData;
 $item = new \CommonsBooking\Model\Item($templateData['item']);
 $hasTimeFrames = (array_key_exists('data', $templateData) && count($templateData['data']));
-$isRoleRestricted = (array_key_exists('data', $templateData));
 
 // the item-not-available message (if item ist currently not available) can be defined via plugin options -> message templates
 $noResultText = \CommonsBooking\Settings\Settings::getOption( COMMONSBOOKING_PLUGIN_SLUG . '_options_templates', 'item-not-available' );
-$restrictedItemText = \CommonsBooking\Settings\Settings::getOption( COMMONSBOOKING_PLUGIN_SLUG . '_options_templates', 'item-booking-restricted' );
-
 
 ?>
 <div class="cb-list-header">
