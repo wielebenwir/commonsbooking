2e1c50147ad3baab0f0b735912343831dcbf586d
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2022-11-11 11:56:43 +0100

    always reset color on update

--- src/Plugin.php
+++ src/Plugin.php
@@ -200,10 +200,9 @@ class Plugin {
 		// check if installed version differs from plugin version in database
 		if (COMMONSBOOKING_VERSION != $commonsbooking_installed_version or !isset($commonsbooking_installed_version)) {
 
-			//reset greyed out color when upgrading to version 2.7.3. from any other version, see issue #1121
-			if (COMMONSBOOKING_VERSION == '2.7.3'){
-				Settings::updateOption( 'commonsbooking_options_templates', 'colorscheme_greyedoutcolor', '#f6f6f6' );
-			}
+			//reset greyed out color when upgrading, see issue #1121
+			Settings::updateOption( 'commonsbooking_options_templates', 'colorscheme_greyedoutcolor', '#f6f6f6' );
+
 			// set Options default values (e.g. if there are new fields added)
 			AdminOptions::SetOptionsDefaultValues();
 
