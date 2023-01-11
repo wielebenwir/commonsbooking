494439f2fe8113666d175c97d603c4ee8fe8d236
HansMorb <6433480+hansmorb@users.noreply.github.com>
2023-01-12 00:06:38 +0100

    unit tests: fix again

--- tests/Service/BookingRuleTest.php
+++ tests/Service/BookingRuleTest.php
@@ -17,7 +17,7 @@ class BookingRuleTest extends CustomPostTypeTest
 	protected BookingRule $alwaysdeny;
 	protected BookingRule $alwaysallow;
 
-	protected int $normalUserID;
+	protected int $normalUserID = 10;
 
 
     public function test__construct()
@@ -40,7 +40,30 @@ class BookingRuleTest extends CustomPostTypeTest
 
 	protected function setUp() {
 		parent::setUp();
-		$this->normalUserID = wp_create_user("Normal","Password");
+		$userdata = array(
+			'ID' 					=> 10, 	//(int) User ID. If supplied, the user will be updated.
+			'user_pass'				=> 'normal', 	//(string) The plain-text user password.
+			'user_login' 			=> 'normal', 	//(string) The user's login username.
+			'user_nicename' 		=> 'normal', 	//(string) The URL-friendly user name.
+			'user_url' 				=> '', 	//(string) The user URL.
+			'user_email' 			=> 'a@a.de', 	//(string) The user email address.
+			'display_name' 			=> 'normal', 	//(string) The user's display name. Default is the user's username.
+			'nickname' 				=> 'normal', 	//(string) The user's nickname. Default is the user's username.
+			'first_name' 			=> '', 	//(string) The user's first name. For new users, will be used to build the first part of the user's display name if $display_name is not specified.
+			'last_name' 			=> '', 	//(string) The user's last name. For new users, will be used to build the second part of the user's display name if $display_name is not specified.
+			'description' 			=> '', 	//(string) The user's biographical description.
+			'rich_editing' 			=> '', 	//(string|bool) Whether to enable the rich-editor for the user. False if not empty.
+			'syntax_highlighting' 	=> '', 	//(string|bool) Whether to enable the rich code editor for the user. False if not empty.
+			'comment_shortcuts' 	=> '', 	//(string|bool) Whether to enable comment moderation keyboard shortcuts for the user. Default false.
+			'admin_color' 			=> 'fresh', 	//(string) Admin color scheme for the user. Default 'fresh'.
+			'use_ssl' 				=> '', 	//(bool) Whether the user should always access the admin over https. Default false.
+			'user_registered' 		=> '', 	//(string) Date the user registered. Format is 'Y-m-d H:i:s'.
+			'show_admin_bar_front' 	=> '', 	//(string|bool) Whether to display the Admin Bar for the user on the site's front end. Default true.
+			'role' 					=> '', 	//(string) User's role.
+			'locale' 				=> '', 	//(string) User's locale. Default empty.
+
+		);
+		wp_insert_user($userdata);
 		$this->timeframeOne = parent::createConfirmedBookingEndingToday();
 		$this->timeframeTwo = parent::createConfirmedBookingStartingToday();
 		$this->testItem     = parent::createItem( 'testitem', 'publish' );
