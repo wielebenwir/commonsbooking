b28daecdf69f55cf7641bc70e5cf9f9d226a8f64
HansMorb <6433480+hansmorb@users.noreply.github.com>
2023-02-01 20:35:13 +0100

    added unit tests for new functions

--- tests/Service/BookingRuleTest.php
+++ tests/Service/BookingRuleTest.php
@@ -2,6 +2,7 @@
 
 namespace CommonsBooking\Tests\Service;
 
+use CommonsBooking\Model\Booking;
 use CommonsBooking\Tests\Wordpress\CustomPostTypeTest;
 use CommonsBooking\Service\BookingRule;
 
@@ -10,6 +11,7 @@ class BookingRuleTest extends CustomPostTypeTest
 	protected $testBooking;
 	protected BookingRule $alwaysdeny;
 	protected BookingRule $alwaysallow;
+	protected int $normalUser;
 
     public function test__construct()
     {
@@ -29,6 +31,106 @@ class BookingRuleTest extends CustomPostTypeTest
 		);
     }
 
+	public function testCheckSimultaneousBookings(){
+		$testBookingOne       = new Booking( get_post( $this->createBooking(
+			$this->locationId,
+			$this->itemId,
+			strtotime( '+1 day', time()),
+			strtotime( '+2 days', time()),
+			'8:00 AM',
+			'12:00 PM',
+			'confirmed',
+			$this->normalUser
+		) ) );
+		$itemtwo = $this->createItem("Item2",'publish');
+		$locationtwo = $this->createLocation("Location2",'publish');
+		$this->secondTimeframeId = $this->createTimeframe(
+			$locationtwo,
+			$itemtwo,
+			strtotime( '-5 days',time()),
+			strtotime( '+90 days', time()),
+		);
+		$testBookingTwo = new Booking(get_post(
+			$this->createBooking(
+				$locationtwo,
+				$itemtwo,
+				strtotime('+1 day', time()),
+				strtotime('+2 days', time()),
+				'8:00 AM',
+				'12:00 PM',
+				'unconfirmed',
+				$this->normalUser
+			)
+		));
+		$this->assertTrue(BookingRule::checkSimultaneousBookings($testBookingTwo));
+		$this->tearDownAllBookings();
+	}
+
+	public function testCheckChainBooking(){
+		$testBookingOne       = new Booking( get_post( $this->createBooking(
+			$this->locationId,
+			$this->itemId,
+			strtotime( '+1 day', time()),
+			strtotime( '+4 days', time()),
+			'8:00 AM',
+			'12:00 PM',
+			'confirmed',
+			$this->normalUser
+		) ) );
+		$testBookingTwo = new Booking(get_post(
+			$this->createBooking(
+				$this->locationId,
+				$this->itemId,
+				strtotime('+4 day', time()),
+				strtotime('+5 days', time()),
+				'8:00 AM',
+				'12:00 PM',
+				'unconfirmed',
+				$this->normalUser
+			)
+		));
+		$this->assertTrue(BookingRule::checkChainBooking($testBookingTwo));
+	}
+
+	public function testCheckMaxBookingDays(){
+		$testBookingOne       = new Booking( get_post( $this->createBooking(
+			$this->locationId,
+			$this->itemId,
+			strtotime( '+1 day', time()),
+			strtotime( '+2 days', time()),
+			'8:00 AM',
+			'12:00 PM',
+			'confirmed',
+			$this->normalUser
+		) ) );
+		$testBookingTwo = new Booking(get_post(
+			$this->createBooking(
+				$this->locationId,
+				$this->itemId,
+				strtotime('+4 day', time()),
+				strtotime('+5 days', time()),
+				'8:00 AM',
+				'12:00 PM',
+				'confirmed',
+				$this->normalUser
+			)
+		));
+
+		$testBookingThree = new Booking(get_post(
+			$this->createBooking(
+				$this->locationId,
+				$this->itemId,
+				strtotime('+6 day', time()),
+				strtotime('+7 days', time()),
+				'8:00 AM',
+				'12:00 PM',
+				'unconfirmed',
+				$this->normalUser
+			)
+		));
+		$this->assertTrue(BookingRule::checkMaxBookingDays($testBookingThree,array(2,30)));
+	}
+
 	protected function setUp() {
 		parent::setUp();
 		$this->alwaysallow = new BookingRule(
@@ -37,7 +139,7 @@ class BookingRuleTest extends CustomPostTypeTest
 			"Rule will always evaluate to true",
 			"Rule did not evaluate to true",
 			function(\CommonsBooking\Model\Booking $booking){
-				return true;
+				return false;
 			}
 		);
 		$this->alwaysdeny = new BookingRule(
@@ -46,13 +148,27 @@ class BookingRuleTest extends CustomPostTypeTest
 			"Rule will always evaluate to false",
 			"Rule evaluated correctly",
 			function(\CommonsBooking\Model\Booking $booking){
-				return false;
+				return true;
 			}
 		);
+		$this->firstTimeframeId   = $this->createTimeframe(
+			$this->locationId,
+			$this->itemId,
+			strtotime( '-5 days',time()),
+			strtotime( '+90 days', time())
+		);
+
+		$wp_user = get_user_by('email',"a@a.de");
+		if (! $wp_user){
+			$this->normalUser = wp_create_user("normaluser","normal","a@a.de");
+		}
+		else {
+			$this->normalUser = $wp_user->ID;
+		}
 
 	}
 
-	protected function tearDown() {
+	protected function tearDown(){
 		parent::tearDown();
 	}
 }
