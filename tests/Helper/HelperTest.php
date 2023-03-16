564ef99851fd92bec9c95c03683f2fb77d1bfe0e
HansMorb <6433480+hansmorb@users.noreply.github.com>
2023-03-16 10:30:41 +0100

    moved helper functions into class

--- tests/Helper/HelperTest.php
+++ tests/Helper/HelperTest.php
@@ -7,33 +7,18 @@ use DateTimeInterface;
 use PHPUnit\Framework\TestCase;
 use CommonsBooking\Helper\Helper;
 
-function dt_to_ts( $time_str ): DateTime {
-	return DateTime::createFromFormat( DateTimeInterface::ATOM, $time_str );
-}
-
-/**
- * Returns date time int
- *
- * @param $time_str string like '2020-01-01T12:00:00+00:00'
- *
- * @return int
- */
-function to_ts( string $time_str ): DateTime {
-	return DateTime::createFromFormat( 'Y-m-d', $time_str );
-}
-
 class HelperTest extends TestCase {
 
 	public function test_whenAllTimeFramesOverlap_returnOneTimeframe() {
 		$arrayOfBookableDates = Helper::merge_ranges_to_bookable_dates(
 			array(
 				array(
-					'start_date' => to_ts( '2020-01-01' ),
-					'end_date'   => to_ts( '2020-01-03' ),
+					'start_date' => self::to_ts( '2020-01-01' ),
+					'end_date'   => self::to_ts( '2020-01-03' ),
 				),
 				array(
-					'start_date' => to_ts( '2020-01-02' ),
-					'end_date'   => to_ts( '2020-01-04' ),
+					'start_date' => self::to_ts( '2020-01-02' ),
+					'end_date'   => self::to_ts( '2020-01-04' ),
 				),
 			)
 		);
@@ -41,8 +26,8 @@ class HelperTest extends TestCase {
 		$this->assertCount( 1, $arrayOfBookableDates );
 		$merged = $arrayOfBookableDates[0];
 
-		$this->assertEquals( $merged['start_date'], to_ts( '2020-01-01' ) );
-		$this->assertEquals( $merged['end_date'], to_ts( '2020-01-04' ) );
+		$this->assertEquals( $merged['start_date'], self::to_ts( '2020-01-01' ) );
+		$this->assertEquals( $merged['end_date'], self::to_ts( '2020-01-04' ) );
 	}
 
 
@@ -50,12 +35,12 @@ class HelperTest extends TestCase {
 		$arrayOfBookableDates = Helper::merge_ranges_to_bookable_dates(
 			array(
 				array(
-					'start_date' => to_ts( '2020-01-01' ),
-					'end_date'   => to_ts( '2020-01-02' ),
+					'start_date' => self::to_ts( '2020-01-01' ),
+					'end_date'   => self::to_ts( '2020-01-02' ),
 				),
 				array(
-					'start_date' => to_ts( '2020-01-02' ),
-					'end_date'   => to_ts( '2020-01-04' ),
+					'start_date' => self::to_ts( '2020-01-02' ),
+					'end_date'   => self::to_ts( '2020-01-04' ),
 				),
 			)
 		);
@@ -63,8 +48,8 @@ class HelperTest extends TestCase {
 		$this->assertCount( 1, $arrayOfBookableDates );
 		$merged = $arrayOfBookableDates[0];
 
-		$this->assertEquals( $merged['start_date'], to_ts( '2020-01-01' ) );
-		$this->assertEquals( $merged['end_date'], to_ts( '2020-01-04' ) );
+		$this->assertEquals( $merged['start_date'], self::to_ts( '2020-01-01' ) );
+		$this->assertEquals( $merged['end_date'], self::to_ts( '2020-01-04' ) );
 	}
 
 
@@ -72,36 +57,36 @@ class HelperTest extends TestCase {
 		$arrayOfBookableDates = Helper::merge_ranges_to_bookable_dates(
 			array(
 				array(
-					'start_date' => dt_to_ts( '2020-01-01T12:00:00+00:00' ),
-					'end_date'   => dt_to_ts( '2020-01-02T12:00:00+00:00' ),
+					'start_date' => self::dt_to_ts( '2020-01-01T12:00:00+00:00' ),
+					'end_date'   => self::dt_to_ts( '2020-01-02T12:00:00+00:00' ),
 				),
 				array(
-					'start_date' => dt_to_ts( '2020-01-03T12:00:00+00:00' ),
-					'end_date'   => dt_to_ts( '2020-01-04T12:00:00+00:00' ),
+					'start_date' => self::dt_to_ts( '2020-01-03T12:00:00+00:00' ),
+					'end_date'   => self::dt_to_ts( '2020-01-04T12:00:00+00:00' ),
 				),
 			)
 		);
 
 		$this->assertCount( 2, $arrayOfBookableDates );
 
-		$this->assertEquals( $arrayOfBookableDates[0]['start_date'], dt_to_ts( '2020-01-01T12:00:00+00:00' ) );
-		$this->assertEquals( $arrayOfBookableDates[1]['end_date'],   dt_to_ts( '2020-01-04T12:00:00+00:00' ) );
+		$this->assertEquals( $arrayOfBookableDates[0]['start_date'], self::dt_to_ts( '2020-01-01T12:00:00+00:00' ) );
+		$this->assertEquals( $arrayOfBookableDates[1]['end_date'],   self::dt_to_ts( '2020-01-04T12:00:00+00:00' ) );
 	}
 
 	public function test_whenThreeTimeFramesOverlap_returnOneTimeframe() {
 		$arrayOfBookableDates = Helper::merge_ranges_to_bookable_dates(
 			array(
 				array(
-					'start_date' => to_ts( '2020-01-01' ),
-					'end_date'   => to_ts( '2020-01-05' ),
+					'start_date' => self::to_ts( '2020-01-01' ),
+					'end_date'   => self::to_ts( '2020-01-05' ),
 				),
 				array(
-					'start_date' => to_ts( '2020-01-02' ),
-					'end_date'   => to_ts( '2020-01-04' ),
+					'start_date' => self::to_ts( '2020-01-02' ),
+					'end_date'   => self::to_ts( '2020-01-04' ),
 				),
 				array(
-					'start_date' => to_ts( '2020-01-05' ),
-					'end_date'   => to_ts( '2020-01-06' ),
+					'start_date' => self::to_ts( '2020-01-05' ),
+					'end_date'   => self::to_ts( '2020-01-06' ),
 				),
 			)
 		);
@@ -109,20 +94,20 @@ class HelperTest extends TestCase {
 		$this->assertCount( 1, $arrayOfBookableDates );
 		$merged = $arrayOfBookableDates[0];
 
-		$this->assertEquals( $merged['start_date'], to_ts( '2020-01-01' ) );
-		$this->assertEquals( $merged['end_date'], to_ts( '2020-01-06' ) );
+		$this->assertEquals( $merged['start_date'], self::to_ts( '2020-01-01' ) );
+		$this->assertEquals( $merged['end_date'], self::to_ts( '2020-01-06' ) );
 	}
 
 	public function test_whenTimeFrameHasOpenInterval_returnOneTimeFrame() {
 		$arrayOfBookableDates = Helper::merge_ranges_to_bookable_dates(
 			array(
 				array(
-					'start_date' => to_ts( '2020-01-01' ),
+					'start_date' => self::to_ts( '2020-01-01' ),
 					'end_date'   => false,
 				),
 				array(
-					'start_date' => to_ts( '2020-01-01' ),
-					'end_date'   => to_ts( '2020-01-04' ),
+					'start_date' => self::to_ts( '2020-01-01' ),
+					'end_date'   => self::to_ts( '2020-01-04' ),
 				),
 			)
 		);
@@ -130,7 +115,7 @@ class HelperTest extends TestCase {
 		$this->assertCount( 1, $arrayOfBookableDates );
 		$merged = $arrayOfBookableDates[0];
 
-		$this->assertEquals( to_ts( '2020-01-01' ), $merged['start_date'] );
+		$this->assertEquals( self::to_ts( '2020-01-01' ), $merged['start_date'] );
 		$this->assertEquals( false, $merged['end_date'] );
 	}
 
@@ -138,11 +123,11 @@ class HelperTest extends TestCase {
 		$arrayOfBookableDates = Helper::merge_ranges_to_bookable_dates(
 			array(
 				array(
-					'start_date' => to_ts( '2020-01-01' ),
-					'end_date'   => to_ts( '2020-04-01' ),
+					'start_date' => self::to_ts( '2020-01-01' ),
+					'end_date'   => self::to_ts( '2020-04-01' ),
 				),
 				array(
-					'start_date' => to_ts( '2020-03-01' ),
+					'start_date' => self::to_ts( '2020-03-01' ),
 					'end_date'   => false,
 				),
 			)
@@ -151,7 +136,7 @@ class HelperTest extends TestCase {
 		$this->assertCount( 1, $arrayOfBookableDates );
 		$merged = $arrayOfBookableDates[0];
 
-		$this->assertEquals( to_ts( '2020-01-01' ), $merged['start_date'] );
+		$this->assertEquals( self::to_ts( '2020-01-01' ), $merged['start_date'] );
 		$this->assertEquals( false, $merged['end_date'] );
 	}
 
@@ -174,5 +159,20 @@ class HelperTest extends TestCase {
 
 		$this->assertEquals( 1673136000, $merged['start_date'] );
 		$this->assertEquals( false, $merged['end_date'] );
+	}
+
+	private function dt_to_ts( $time_str ): DateTime {
+		return DateTime::createFromFormat( DateTimeInterface::ATOM, $time_str );
+	}
+
+	/**
+	 * Returns date time object from string
+	 *
+	 * @param $time_str string like '2020-01-01T12:00:00+00:00'
+	 *
+	 * @return \DateTime
+	 */
+	private function to_ts( string $time_str ): DateTime {
+		return DateTime::createFromFormat( 'Y-m-d', $time_str );
 	}
 }
