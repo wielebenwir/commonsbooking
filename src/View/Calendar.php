f059eff4eb000fdeaa71f89a6ad12ee4960068e9
christian-wlw <github@farbschein.de>
2023-03-22 00:35:04 +0100

    fixed parse error

--- src/View/Calendar.php
+++ src/View/Calendar.php
@@ -449,7 +449,7 @@ class Calendar {
 		array $items,
 		$advanceBookingDays = null,
 		$lastBookableDate = null,
-        $firstBookableDay = null,
+        $firstBookableDay = null
 	): array {
 
         $current_user   = wp_get_current_user();
