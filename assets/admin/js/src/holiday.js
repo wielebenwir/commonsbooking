7db9a54201a7922a84ed3f9fbfda65fb1405b0d1
HansMorb <6433480+hansmorb@users.noreply.github.com>
2023-04-17 20:22:22 +0200

    formatted holidays correctly so manual rep can read it

--- assets/admin/js/src/holiday.js
+++ assets/admin/js/src/holiday.js
@@ -5,22 +5,24 @@
             var fillHolidays = (year, state) => {
                 var holidays = feiertagejs.getHolidays(year, state);
                 const inputField = $("#timeframe_manual_date");
+                const DATE_SEPERATOR = ",";
                 //add holidays to input field, comma separated in format (d.m.Y) and with trailing semi-colon
                 holidays.forEach((holiday) => {
                     var date = new Date(holiday.date);
-                    var day = date.getDate();
+                    //we need to add a leading zero if the day or month is less than 10
+                    var dd = (date.getDate()).length == 1 ? "0" + date.getDate() : date.getDate();
                     //month is 0 based, so we need to add 1
-                    var month = date.getMonth() + 1;
-                    var year = date.getFullYear();
-                    var dateStr = day + "." + month + "." + year;
+                    var mm = (date.getMonth() + 1).length == 1 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
+                    var yyyy = date.getFullYear();
+                    var dateStr = yyyy + "-" + mm + "-" + dd;
                     if (inputField.val().length > 0) {
-                        if (inputField.val().slice(-1) !== ";") {
-                            inputField.val(inputField.val() + ";" + dateStr);
+                        if (inputField.val().slice(-1) !== DATE_SEPERATOR) {
+                            inputField.val(inputField.val() + DATE_SEPERATOR + dateStr);
                         } else {
                             inputField.val(inputField.val() + dateStr);
                         }
                     } else {
-                        inputField.val(dateStr + ";");
+                        inputField.val(dateStr + DATE_SEPERATOR);
                     }
                 });
             };
