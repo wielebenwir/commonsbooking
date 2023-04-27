007fabde9fa0eca12a78260ffaeb9d07f0656dcc
HansMorb <6433480+hansmorb@users.noreply.github.com>
2023-04-27 20:16:42 +0200

    grunt after merge

--- assets/admin/js/admin.js
+++ assets/admin/js/admin.js
@@ -132,7 +132,6 @@
             const bookingConfigTitle = $(".cmb2-id-title-bookings-config");
             const maxDaysSelect = $(".cmb2-id-timeframe-max-days");
             const advanceBookingDays = $(".cmb2-id-timeframe-advance-booking-days");
-            const BookingStartDayOffset = $(".cmb2-id-booking-startday-offset");
             const allowUserRoles = $(".cmb2-id-allowed-user-roles");
             const repSet = [ repConfigTitle, fullDayInput, startTimeInput, endTimeInput, weekdaysInput, repetitionStartInput, repetitionEndInput, gridInput ];
             const noRepSet = [ fullDayInput, startTimeInput, endTimeInput, gridInput, repetitionStartInput, repetitionEndInput ];
