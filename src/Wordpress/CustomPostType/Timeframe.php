bacef9a08ead24391be64f0872513ece3cbbf47a
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-06-24 19:50:57 +0200

    fixed label

--- src/Wordpress/CustomPostType/Timeframe.php
+++ src/Wordpress/CustomPostType/Timeframe.php
@@ -642,7 +642,7 @@ class Timeframe extends CustomPostType {
 				'name' => esc_html__( "Booking Codes", 'commonsbooking' ),
 				'desc' => commonsbooking_sanitizeHTML( __( 'You can automatically generate booking codes. Codes can be generated only with the following settings:</br>
 				- Whole day is enabled</br>
-				- Start date and end date are set</br>
+				- Timeframe is bookable</br>
 				<a href="https://commonsbooking.org/?p=437" target="_blank">More Information in the documentation</a>
 				', 'commonsbooking' ) ),
 				'id'   => "title-timeframe-booking-codes",
