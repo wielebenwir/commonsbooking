aa037692d1ec29321526f4e92198580ee7064f9e
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-07-27 21:16:58 +0200

    removed comment

--- src/Wordpress/CustomPostType/Booking.php
+++ src/Wordpress/CustomPostType/Booking.php
@@ -784,19 +784,7 @@ class Booking extends Timeframe {
 			$dateFormat = 'm/d/Y';
 		}
 
-		$userOptions = [];
-
-        // Generate user list for admin bookings
-		// if ( commonsbooking_isCurrentUserAdmin() || commonsbooking_isCurrentUserCBManager() ) {
-		// 	$users       = get_users();
-		// 	$userOptions = [];
-		// 	foreach ( $users as $user ) {
-		// 		$userOptions[ $user->ID ] = $user->get( 'user_nicename' ) . ' (' . $user->first_name . ' ' . $user->last_name . ')';
-		// 	}
-		// }
-
         $booking_user = get_user_by('ID', get_current_user_id());
-
 
         // define form fields based on CMB2
 		return array(
