e20945097e1c0ce6c4d5397f74b34ca4916b0daf
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-08-30 22:26:25 -0400

    added tests to load every page

--- cypress/e2e/load-wp.cy.js
+++ cypress/e2e/load-wp.cy.js
@@ -8,7 +8,7 @@ describe( 'Run a pull', function() {
 		cy.get( '#wp-submit' ).click();
 	} );
 	
-	it( 'can run a pull', function() {
+	it( 'can open WP dashboard', function() {
 		cy.wait( 2000 );
 		cy.url().should('eq', 'http://localhost:1001/wp-admin/');
         // Assert that the page title contains "Dashboard"
@@ -17,4 +17,46 @@ describe( 'Run a pull', function() {
         // Assert that the "Welcome to WordPress!" message is present
         cy.contains('Welcome to WordPress!');
 	} );
+
+    it ('can load the CB pages', function() {
+        cy.visit( '/wp-admin/admin.php?page=cb-dashboard' );
+        cy.contains("Welcome to CommonsBooking.");
+
+        //Timeframes menu page
+        cy.visit( '/wp-admin/edit.php?post_type=cb_timeframe' );
+        cy.contains("Timeframes");
+
+        //Items menu page
+        cy.visit( '/wp-admin/edit.php?post_type=cb_item' );
+        cy.contains("Items");
+
+        //Locations menu page
+        cy.visit( '/wp-admin/edit.php?post_type=cb_location' );
+        cy.contains("Locations");
+
+        //Bookings menu page
+        cy.visit( '/wp-admin/edit.php?post_type=cb_booking' );
+        cy.contains("Bookings");
+
+        //Maps menu page
+        cy.visit( '/wp-admin/edit.php?post_type=cb_map' );
+        cy.contains("Maps");
+
+        //Restrictions menu page
+        cy.visit( '/wp-admin/edit.php?post_type=cb_restriction' );
+        cy.contains("Restrictions");
+
+        //Item Categories menu page
+        cy.visit( '/wp-admin/edit-tags.php?taxonomy=cb_items_category' );
+        cy.contains("Item Category");
+
+        //Location Categories menu page
+        cy.visit( '/wp-admin/edit-tags.php?taxonomy=cb_locations_category' );
+        cy.contains("Location Category");
+
+        //Settings page
+        cy.visit( '/wp-admin/options-general.php?page=commonsbooking_options' );
+        cy.contains("Welcome to CommonsBooking");
+
+    } );
 });
