0bb523414103dfbcb0b9b64ac1cf370e37f1e36b
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-09-02 19:38:38 -0400

    exempt total breakdown bookings from reminders & post-message

--- src/Service/Booking.php
+++ src/Service/Booking.php
@@ -70,6 +70,9 @@ class Booking {
 
 		if ( count( $bookings ) ) {
 			foreach ( $bookings as $booking ) {
+				if ( $booking->hasTotalBreakdown() ) {
+					continue;
+				}
 				$reminderMessage = new BookingReminderMessage( $booking->getPost()->ID, 'pre-booking-reminder' );
 				$reminderMessage->sendMessage();
 			}
@@ -109,6 +112,9 @@ class Booking {
 
 		if ( count( $bookings ) ) {
 			foreach ( $bookings as $booking ) {
+				if ( $booking->hasTotalBreakdown() ) {
+					continue;
+				}
 				$reminderMessage = new BookingReminderMessage( $booking->getPost()->ID, 'post-booking-notice' );
 				$reminderMessage->sendMessage();
 			}
