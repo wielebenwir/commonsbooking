ec6b7467233ba8cbcfec3aa75e83a9bf1ef22da4
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-09-27 17:11:06 -0400

    refactored overbooking tests

--- tests/cypress/e2e/litepicker-overbooking.cy.js
+++ tests/cypress/e2e/litepicker-overbooking.cy.js
@@ -1,4 +1,62 @@
 describe('test overbooking process', () => {
+
+    function visitTestPage(testName, itemFixtures, locationFixtures){
+        let item = itemFixtures.find( (item) => {
+            return item.test_name === testName
+        })
+        //get post_name and post_id
+        let itemPostName = item.post_name
+        let locationPostId = getLocIDForTest(testName, locationFixtures)
+        cy.visit( '/?cb_item=' + itemPostName + '&cb-location=' + locationPostId)
+    }
+
+    function getLocIDForTest(testName, locationFixtures){
+        let location = locationFixtures.find( (location) => {
+            return location.test_name === testName
+        })
+        return location.post_id
+    }
+
+    function updatePostMetaAndReload(postID, metaKey, metaValue){
+        cy.exec('bin/wp-env-cli tests-wordpress "wp --allow-root post meta update "' + postID + '" ' + metaKey + ' ' + metaValue + '"')
+        cy.reload()
+    }
+
+    //Will check, if the f
+    function assertFridayAndMondayAreClickable( ) {
+        //assert if the monday and friday dates are clickable (does not mean that they are overbookable)
+        //This should always be the case if the timeframes are configured correctly
+        //check that everything is clickable, including the monday while it is not selected
+        cy.get('.is-today').should('not.have.class','is-locked')
+        //and that the monday is clickable too
+        cy.get('.is-today').next('.day-item').next('.day-item').next('.day-item').should('not.have.class', 'is-locked')
+    }
+
+    function assertFridayAndMondayBookable ( shouldbeBookable, unbookableClass = 'is-locked' ) {
+        //assert, if the monday & friday dates are bookable (meaning you can click them and proceed to booking)
+        cy.get('.is-today').click();
+        //the saturday should not be clickable
+        cy.get('.is-start-date').next('.day-item').should('have.class', unbookableClass)
+        //and the sunday also not
+        cy.get('.is-start-date').next('.day-item').next('.day-item').should('have.class', unbookableClass)
+        //the monday
+        if (shouldbeBookable){
+            cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').should('not.have.class', 'is-locked')
+            //but the tuesday should not be clickable in all test because we always count at least one day
+            cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').next('.day-item').should('have.class', 'is-locked')
+            //and we should be able to proceed booking
+            cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').click();
+            cy.get('#booking-form > [type="submit"]').should('not.be.disabled');
+        }
+        else {
+            cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').should('have.class', 'is-locked')
+            //try to click it anyway and assert that we can't proceed booking
+            cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').click();
+            cy.get('#booking-form > [type="submit"]').should('be.disabled');
+        }
+    }
+
+
     beforeEach( function() {
         //freeze our date to the 6th of November 2023, a Friday
         cy.clock(new Date(2023, 9, 6).getTime());
@@ -18,226 +76,78 @@ describe('test overbooking process', () => {
 
   it('WeeklyRepetition is not overbookable works', function () {
       const testName = 'WeeklyRepetition NoOverbooking'
-      let item = this.bookableItems.find( (item) => {
-          return item.test_name === testName
-      })
-      let location = this.bookableLocations.find( (location) => {
-          return location.test_name === testName
-      })
-      //get post_name and post_id
-      let itemPostName = item.post_name
-      let locationPostId = location.post_id
-      cy.visit( '/?cb_item=' + itemPostName + '&cb-location=' + locationPostId)
-      //check that everything is clickable, including the monday while it is not selected
-      cy.get('.is-today').should('not.have.class','is-locked')
-      //and that the monday is clickable too
-      cy.get('.is-today').next('.day-item').next('.day-item').next('.day-item').should('not.have.class', 'is-locked')
-
-
-      cy.get('.is-today').click();
-      //the next days should not be clickable
-      cy.get('.is-start-date').next('.day-item').should('have.class', 'is-locked')
-      //and the sunday also not
-      cy.get('.is-start-date').next('.day-item').next('.day-item').should('have.class', 'is-locked')
-      //as well as the monday
-      cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').should('have.class', 'is-locked')
-      //try to click it anyway and assert that we can't proceed booking
-      cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').click();
-      cy.get('#booking-form > [type="submit"]').should('be.disabled');
-
+      visitTestPage(testName, this.bookableItems, this.bookableLocations)
+      assertFridayAndMondayAreClickable(true)
+      assertFridayAndMondayBookable(false)
   })
     it("Weekly Repetition is overbookable count each day works", function () {
       //initially, overbooking should not work becasue the maximum bookable days are 3
-        const testName = 'WeeklyRepetition Overbooking CountAll'
-        //TODO: Refactor duplicate code
-        let item = this.bookableItems.find( (item) => {
-            return item.test_name === testName
-        })
-        let location = this.bookableLocations.find( (location) => {
-            return location.test_name === testName
-        })
-        //get post_name and post_id
-        let itemPostName = item.post_name
-        let locationPostId = location.post_id
         let timeframeID = 51 //TODO: get this from fixtures
-        cy.visit( '/?cb_item=' + itemPostName + '&cb-location=' + locationPostId)
-        //perform just the same assertions as in the first test, then change parameters
-        //check that everything is clickable, including the monday while it is not selected
-        cy.get('.is-today').should('not.have.class','is-locked')
-        //and that the monday is clickable too
-        cy.get('.is-today').next('.day-item').next('.day-item').next('.day-item').should('not.have.class', 'is-locked')
-        cy.get('.is-today').click();
-        //the next days should not be clickable
-        cy.get('.is-start-date').next('.day-item').should('have.class', 'is-locked')
-        //and the sunday also not
-        cy.get('.is-start-date').next('.day-item').next('.day-item').should('have.class', 'is-locked')
-        //as well as the monday
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').should('have.class', 'is-locked')
-        //try to click it anyway and assert that we can't proceed booking
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').click();
-        cy.get('#booking-form > [type="submit"]').should('be.disabled');
+        const testName = 'WeeklyRepetition Overbooking CountAll'
+        visitTestPage(testName, this.bookableItems, this.bookableLocations)
+        assertFridayAndMondayAreClickable()
+        assertFridayAndMondayBookable(false)
 
-        //now change the parameters
-        cy.exec('bin/wp-env-cli tests-wordpress "wp --allow-root post meta update "' + timeframeID + '" timeframe-max-days 4"')
-        //and reload the page
-        cy.reload()
-        //now the monday should be clickable after selection
-        cy.get('.is-today').click();
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').should('not.have.class', 'is-locked')
-        //and we should be able to proceed booking
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').click();
-        cy.get('#booking-form > [type="submit"]').should('not.be.disabled');
+        //lets raise the max days
+        updatePostMetaAndReload(timeframeID, 'timeframe-max-days', 4)
 
+        //now the monday should be bookable after selection
+        assertFridayAndMondayAreClickable()
+        assertFridayAndMondayBookable(true)
         //change it back to three so we can re-run the test
-        cy.exec('bin/wp-env-cli tests-wordpress "wp --allow-root post meta update "' + timeframeID + '" timeframe-max-days 3"')
+        updatePostMetaAndReload(timeframeID, 'timeframe-max-days', 3)
     })
 
     it("Weekly Repetition is overbookable count block of days works", function () {
         const testName = 'WeeklyRepetition Overbooking CountOne'
-        let item = this.bookableItems.find( (item) => {
-            return item.test_name === testName
-        })
-        let location = this.bookableLocations.find( (location) => {
-            return location.test_name === testName
-        })
-        //get post_name and post_id
-        let itemPostName = item.post_name
-        let locationPostId = location.post_id
-        cy.visit( '/?cb_item=' + itemPostName + '&cb-location=' + locationPostId)
-        //check that everything is clickable, including the monday while it is not selected
-        cy.get('.is-today').should('not.have.class','is-locked')
-        //and that the monday is clickable too
-        cy.get('.is-today').next('.day-item').next('.day-item').next('.day-item').should('not.have.class', 'is-locked')
-
-        cy.get('.is-today').click();
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').should('not.have.class', 'is-locked')
-        //but the tuesday should not be clickable because we count one day
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').next('.day-item').should('have.class', 'is-locked')
-        //and we should be able to proceed booking
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').click();
-        cy.get('#booking-form > [type="submit"]').should('not.be.disabled');
+        visitTestPage(testName, this.bookableItems, this.bookableLocations)
+        assertFridayAndMondayAreClickable()
+        //in this default setup, only one day of the weekend is counted so that we can book up to the monday (but not the tuesday)
+        assertFridayAndMondayBookable(true)
 
         //now, let's raise the block counter to two days and assert that everything becomes unclickable again
-        cy.exec('bin/wp-env-cli tests-wordpress "wp --allow-root post meta update "' + locationPostId + '" _cb_count_lockdays_maximum 2"')
-        cy.reload()
-        cy.get('.is-today').click();
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').should('have.class', 'is-locked')
+        updatePostMetaAndReload(getLocIDForTest(testName,this.bookableLocations), '_cb_count_lockdays_maximum', 2)
+
+        assertFridayAndMondayBookable(false)
 
         //reset, so that we can run the tests again
-        cy.exec('bin/wp-env-cli tests-wordpress "wp --allow-root post meta update "' + locationPostId + '" _cb_count_lockdays_maximum 1"')
-
+        updatePostMetaAndReload(getLocIDForTest(testName,this.bookableLocations), '_cb_count_lockdays_maximum', 1)
     })
 
     it('Daily repetition /w Holiday on weekend no overbooking works', function () {
         const testName = 'DailyRep Holiday NoOverbooking'
-        let item = this.bookableItems.find( (item) => {
-            return item.test_name === testName
-        })
-        let location = this.bookableLocations.find( (location) => {
-            return location.test_name === testName
-        })
-        //get post_name and post_id
-        let itemPostName = item.post_name
-        let locationPostId = location.post_id
-        cy.visit( '/?cb_item=' + itemPostName + '&cb-location=' + locationPostId)
-        //check that everything is clickable, including the monday while it is not selected
-        cy.get('.is-today').should('not.have.class','is-locked')
-        //and that the monday is clickable too
-        cy.get('.is-today').next('.day-item').next('.day-item').next('.day-item').should('not.have.class', 'is-locked')
-
-
-        cy.get('.is-today').click();
-        //the next days should not be clickable
-        cy.get('.is-start-date').next('.day-item').should('have.class', 'is-holiday')
-        //and the sunday also not
-        cy.get('.is-start-date').next('.day-item').next('.day-item').should('have.class', 'is-holiday')
-        //as well as the monday
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').should('have.class', 'is-locked')
-        //try to click it anyway and assert that we can't proceed booking
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').click();
-        cy.get('#booking-form > [type="submit"]').should('be.disabled');
+        visitTestPage(testName, this.bookableItems, this.bookableLocations)
+        assertFridayAndMondayAreClickable()
+        assertFridayAndMondayBookable(false,'is-holiday')
 
     })
     it("Daily repetition /w Holiday on weekend count each day works", function () {
         //initially, overbooking should not work becasue the maximum bookable days are 3
         const testName = 'DailyRep Holiday Overbooking CountAll'
-        //TODO: Refactor duplicate code
-        let item = this.bookableItems.find( (item) => {
-            return item.test_name === testName
-        })
-        let location = this.bookableLocations.find( (location) => {
-            return location.test_name === testName
-        })
-        //get post_name and post_id
-        let itemPostName = item.post_name
-        let locationPostId = location.post_id
         let timeframeID = 75 //TODO: get this from fixtures
-        cy.visit( '/?cb_item=' + itemPostName + '&cb-location=' + locationPostId)
-        //perform just the same assertions as in the first test, then change parameters
-        //check that everything is clickable, including the monday while it is not selected
-        cy.get('.is-today').should('not.have.class','is-locked')
-        //and that the monday is clickable too
-        cy.get('.is-today').next('.day-item').next('.day-item').next('.day-item').should('not.have.class', 'is-locked')
-        cy.get('.is-today').click();
-        //the next days should not be clickable
-        cy.get('.is-start-date').next('.day-item').should('have.class', 'is-holiday')
-        //and the sunday also not
-        cy.get('.is-start-date').next('.day-item').next('.day-item').should('have.class', 'is-holiday')
-        //as well as the monday
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').should('have.class', 'is-locked')
-        //try to click it anyway and assert that we can't proceed booking
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').click();
-        cy.get('#booking-form > [type="submit"]').should('be.disabled');
+        visitTestPage(testName, this.bookableItems, this.bookableLocations)
+        assertFridayAndMondayAreClickable()
+        assertFridayAndMondayBookable(false,'is-holiday')
 
-        //now change the parameters
-        cy.exec('bin/wp-env-cli tests-wordpress "wp --allow-root post meta update "' + timeframeID + '" timeframe-max-days 4"')
-        //and reload the page
-        cy.reload()
-        //now the monday should be clickable after selection
-        cy.get('.is-today').click();
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').should('not.have.class', 'is-locked')
-        //and we should be able to proceed booking
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').click();
-        cy.get('#booking-form > [type="submit"]').should('not.be.disabled');
-
+        //lets raise the max days
+        updatePostMetaAndReload(timeframeID, 'timeframe-max-days', 4)
+        assertFridayAndMondayBookable(true,'is-holiday')
         //change it back to three so we can re-run the test
-        cy.exec('bin/wp-env-cli tests-wordpress "wp --allow-root post meta update "' + timeframeID + '" timeframe-max-days 3"')
+        updatePostMetaAndReload(timeframeID, 'timeframe-max-days', 3)
     })
 
     it("Daily repetition /w Holiday on weekend overbooking count block of days works", function () {
         const testName = 'DailyRep Holiday Overbooking CountOne'
-        let item = this.bookableItems.find( (item) => {
-            return item.test_name === testName
-        })
-        let location = this.bookableLocations.find( (location) => {
-            return location.test_name === testName
-        })
-        //get post_name and post_id
-        let itemPostName = item.post_name
-        let locationPostId = location.post_id
-        cy.visit( '/?cb_item=' + itemPostName + '&cb-location=' + locationPostId)
-        //check that everything is clickable, including the monday while it is not selected
-        cy.get('.is-today').should('not.have.class','is-locked')
-        //and that the monday is clickable too
-        cy.get('.is-today').next('.day-item').next('.day-item').next('.day-item').should('not.have.class', 'is-locked')
-
-        cy.get('.is-today').click();
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').should('not.have.class', 'is-locked')
-        //but the tuesday should not be clickable because we count one day
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').next('.day-item').should('have.class', 'is-locked')
-        //and we should be able to proceed booking
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').click();
-        cy.get('#booking-form > [type="submit"]').should('not.be.disabled');
+        visitTestPage(testName, this.bookableItems, this.bookableLocations)
+        assertFridayAndMondayAreClickable()
+        assertFridayAndMondayBookable(true,'is-holiday')
 
         //now, let's raise the block counter to two days and assert that everything becomes unclickable again
-        cy.exec('bin/wp-env-cli tests-wordpress "wp --allow-root post meta update "' + locationPostId + '" _cb_count_lockdays_maximum 2"')
-        cy.reload()
-        cy.get('.is-today').click();
-        cy.get('.is-start-date').next('.day-item').next('.day-item').next('.day-item').should('have.class', 'is-locked')
+        updatePostMetaAndReload(getLocIDForTest(testName,this.bookableLocations), '_cb_count_lockdays_maximum', 2)
+        assertFridayAndMondayBookable(false,'is-holiday')
 
         //reset, so that we can run the tests again
-        cy.exec('bin/wp-env-cli tests-wordpress "wp --allow-root post meta update "' + locationPostId + '" _cb_count_lockdays_maximum 1"')
-
+        updatePostMetaAndReload(getLocIDForTest(testName,this.bookableLocations), '_cb_count_lockdays_maximum', 1)
     })
 
 
