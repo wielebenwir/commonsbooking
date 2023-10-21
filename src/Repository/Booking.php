060e6f923fc72479485492e10ba0db30ba517759
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-10-21 14:32:55 -0400

    fixed up annotations

--- src/Repository/Booking.php
+++ src/Repository/Booking.php
@@ -383,7 +383,7 @@ class Booking extends PostRepository {
 	 * @param int $perPage The number of bookings per page. A lower number will result in faster queries.
 	 * @param array $customArgs Valid WP_Query args array.
 	 *
-	 * @return array
+	 * @return Booking[] An array of Booking models.
 	 */
 	public static function getForUserPaginated(
 		\WP_User $user,
