4f6354cde0bc0cae05076cf9f9d2e29596dbb86a
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-10-27 16:41:36 -0400

    added metabox creation for CPT to e2e tests

---
+++ tests/cypress/e2e/cpt-creation.cy.js
@@ -0,0 +1,52 @@
+export function clickIfExist(element) {
+    cy.get('body').then((body) => {
+        if (body.find(element).length > 0) {
+            cy.get(element).click();
+        }
+    });
+}
+
+describe('correctly render metaboxes for backend CPT creation', () => {
+   beforeEach( function() {
+        cy.visit( '/wp-login.php' );
+        cy.wait( 1000 );
+        cy.get( '#user_login' ).type( Cypress.env( "wpAdmin" ) );
+        cy.get( '#user_pass' ).type( Cypress.env( "wpPassword" ) );
+        cy.get( '#wp-submit' ).click();
+    } );
+
+    it('shows item metaboxes', () => {
+        cy.visit( '/wp-admin/edit.php?post_type=cb_item' );
+        //dismiss message about block editor
+        cy.get('.page-title-action').click();
+        //dismiss that annoying message about the block editor
+        cy.wait(5000);
+        clickIfExist('.components-modal__header > .components-button');
+        cy.wait(1000);
+        //Just assert, that the CMB2 wrapper is visible, the rest is done through screenshots
+        cy.get('.cmb2-wrap').should('be.visible');
+        cy.screenshot('cb-item-metaboxes')
+    })
+
+    it('shows location metaboxes', () => {
+        cy.visit( '/wp-admin/edit.php?post_type=cb_location' );
+        cy.get('.page-title-action').click();
+        cy.get('#_cb_location_adress').should('exist');
+        cy.get('#_cb_location_info').should('exist');
+        cy.screenshot('cb-location-metaboxes')
+    })
+
+    it('shows timeframe metaboxes', () => {
+        cy.visit( '/wp-admin/edit.php?post_type=cb_timeframe' );
+        const BOOKABLE_ID = "2";
+        const HOLIDAY_ID = "3";
+        const REPAIR_ID = "5";
+        cy.get('.page-title-action').click();
+        cy.get('#type').select(BOOKABLE_ID);
+        cy.screenshot('cb-timeframe-metaboxes-bookable')
+        cy.get('#type').select(HOLIDAY_ID);
+        cy.screenshot('cb-timeframe-metaboxes-holiday')
+        cy.get('#type').select(REPAIR_ID);
+        cy.screenshot('cb-timeframe-metaboxes-repair')
+    })
+})
