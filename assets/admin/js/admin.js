e2499d5e454a29aeff6c9cdd2d979479c8015460
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-10-30 19:46:19 -0400

    run grunt after merge

--- assets/admin/js/admin.js
+++ assets/admin/js/admin.js
@@ -1,6 +1,40 @@
 (function($) {
     "use strict";
     $(function() {
+        const manualDateInput = $("#timeframe_manual_date");
+        const manualDatePicker = $("#cmb2_multiselect_datepicker");
+        var addHolidayToInput = date => {
+            const DATES_SEPERATOR = ",";
+            var day = date.getDate();
+            var month = date.getMonth() + 1;
+            var dd = day <= 9 ? "0" + day : day;
+            var mm = month <= 9 ? "0" + month : month;
+            var yyyy = date.getFullYear();
+            var dateStr = yyyy + "-" + mm + "-" + dd;
+            if (manualDateInput.val().length > 0) {
+                if (manualDateInput.val().slice(-1) !== DATES_SEPERATOR) {
+                    manualDateInput.val(manualDateInput.val() + DATES_SEPERATOR + dateStr);
+                } else {
+                    manualDateInput.val(manualDateInput.val() + dateStr);
+                }
+            } else {
+                manualDateInput.val(dateStr + DATES_SEPERATOR);
+            }
+        };
+        if (manualDatePicker.length) {
+            manualDatePicker.datepicker({
+                onSelect: function(dateText, inst) {
+                    var date = $(this).datepicker("getDate");
+                    addHolidayToInput(date);
+                }
+            });
+        }
+    });
+})(jQuery);
+
+(function($) {
+    "use strict";
+    $(function() {
         const hideFieldset = function(set) {
             $.each(set, function() {
                 $(this).parents(".cmb-row").hide();
@@ -173,6 +207,15 @@
             });
         };
         const timeframeForm = $("#cmb2-metabox-cb_timeframe-custom-fields");
+        const BOOKABLE_ID = "2";
+        const HOLIDAYS_ID = "3";
+        const REPAIR_ID = "5";
+        const REPETITION_NONE = "norep";
+        const REPETITION_MANUAL = "manual";
+        const REPETITION_DAILY = "d";
+        const REPETITION_WEEKLY = "w";
+        const REPETITION_MONTHLY = "m";
+        const REPETITION_YEARLY = "y";
         if (timeframeForm.length) {
             const timeframeRepetitionInput = $("#timeframe-repetition");
             const typeInput = $("#type");
@@ -184,20 +227,24 @@
             const repetitionStartInput = $("#repetition-start");
             const repetitionEndInput = $("#repetition-end");
             const fullDayInput = $("#full-day");
+            const bookingCodeTitle = $("#title-timeframe-booking-codes");
             const showBookingCodes = $("#show-booking-codes");
             const createBookingCodesInput = $("#create-booking-codes");
             const bookingCodesDownload = $("#booking-codes-download");
             const bookingCodesList = $("#booking-codes-list");
-            const bookingConfigTitle = $(".cmb2-id-title-bookings-config");
-            const maxDaysSelect = $(".cmb2-id-timeframe-max-days");
-            const advanceBookingDays = $(".cmb2-id-timeframe-advance-booking-days");
-            const bookingStartDayOffset = $(".cmb2-id-booking-startday-offset");
-            const allowUserRoles = $(".cmb2-id-allowed-user-roles");
+            const holidayInput = $("#timeframe_manual_date");
+            const manualDatePicker = $("#cmb2_multiselect_datepicker");
+            const manualDateField = $(".cmb2-id-timeframe-manual-date");
+            const maxDaysSelect = $("#timeframe-max-days");
+            const advanceBookingDays = $("#timeframe-advance-booking-days");
+            const bookingStartDayOffset = $("#booking-startday-offset");
+            const bookingConfigurationTitle = $("#title-bookings-config");
+            const allowUserRoles = $("#allowed_user_roles");
             const repSet = [ repConfigTitle, fullDayInput, startTimeInput, endTimeInput, weekdaysInput, repetitionStartInput, repetitionEndInput, gridInput ];
             const noRepSet = [ fullDayInput, startTimeInput, endTimeInput, gridInput, repetitionStartInput, repetitionEndInput ];
             const repTimeFieldsSet = [ gridInput, startTimeInput, endTimeInput ];
             const bookingCodeSet = [ createBookingCodesInput, bookingCodesList, bookingCodesDownload, showBookingCodes ];
-            const bookingSettings = [ bookingConfigTitle, maxDaysSelect, advanceBookingDays, bookingStartDayOffset, allowUserRoles ];
+            const bookingConfigSet = [ maxDaysSelect, advanceBookingDays, bookingStartDayOffset, allowUserRoles, bookingConfigurationTitle ];
             const showRepFields = function() {
                 showFieldset(repSet);
                 hideFieldset(arrayDiff(repSet, noRepSet));
@@ -213,14 +260,13 @@
             };
             const handleTypeSelection = function() {
                 const selectedType = $("option:selected", typeInput).val();
-                if (selectedType == 2) {
-                    $.each(bookingSettings, function() {
-                        $(this).show();
-                    });
+                const selectedRepetition = $("option:selected", timeframeRepetitionInput).val();
+                if (selectedType === BOOKABLE_ID) {
+                    showFieldset(bookingConfigSet);
+                    showFieldset(bookingCodeTitle);
                 } else {
-                    $.each(bookingSettings, function() {
-                        $(this).hide();
-                    });
+                    hideFieldset(bookingConfigSet);
+                    hideFieldset(bookingCodeTitle);
                 }
             };
             handleTypeSelection();
@@ -231,7 +277,6 @@
                 const selectedRep = $("option:selected", timeframeRepetitionInput).val();
                 if (fullDayInput.prop("checked")) {
                     gridInput.prop("selected", false);
-                    gridInput.val(0);
                     hideFieldset(repTimeFieldsSet);
                 } else {
                     showFieldset(repTimeFieldsSet);
@@ -243,13 +288,25 @@
             });
             const handleRepetitionSelection = function() {
                 const selectedType = $("option:selected", timeframeRepetitionInput).val();
+                const selectedTimeframeType = $("option:selected", typeInput).val();
                 if (selectedType) {
-                    if (selectedType == "norep") {
+                    if (selectedType == REPETITION_NONE) {
                         showNoRepFields();
                     } else {
                         showRepFields();
                     }
-                    if (selectedType == "w") {
+                    if (selectedType === REPETITION_MANUAL) {
+                        manualDateField.show();
+                        manualDatePicker.show();
+                        hideFieldset(repetitionStartInput);
+                        hideFieldset(repetitionEndInput);
+                    } else {
+                        manualDateField.hide();
+                        manualDatePicker.hide();
+                        showFieldset(repetitionStartInput);
+                        showFieldset(repetitionEndInput);
+                    }
+                    if (selectedType === REPETITION_WEEKLY) {
                         weekdaysInput.parents(".cmb-row").show();
                     } else {
                         weekdaysInput.parents(".cmb-row").hide();
@@ -266,9 +323,9 @@
                 handleRepetitionSelection();
             });
             const handleBookingCodesSelection = function() {
-                const fullday = fullDayInput.prop("checked"), type = typeInput.val(), repStart = repetitionStartInput.val(), repEnd = repetitionEndInput.val();
+                const fullday = fullDayInput.prop("checked"), type = typeInput.val(), repStart = repetitionStartInput.val();
                 hideFieldset(bookingCodeSet);
-                if (repStart && fullday && type == 2) {
+                if (repStart && fullday && type === BOOKABLE_ID) {
                     showFieldset(bookingCodeSet);
                     if (!createBookingCodesInput.prop("checked")) {
                         hideFieldset([ showBookingCodes ]);
