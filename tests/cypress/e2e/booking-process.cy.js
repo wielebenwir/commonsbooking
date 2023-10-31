857bef0de792b7c172a987a158b7edce59c2ac45
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-10-31 15:35:04 -0400

    fix e2e tests for last day of month #1392

--- tests/cypress/e2e/booking-process.cy.js
+++ tests/cypress/e2e/booking-process.cy.js
@@ -1,12 +1,27 @@
 describe('test booking process', () => {
-    beforeEach( function() {
-        cy.visit( '/wp-login.php' );
-        cy.wait( 1000 );
-        cy.get( '#user_login' ).type( Cypress.env( "wpSubscriber" ) );
-        cy.get( '#user_pass' ).type( Cypress.env( "wpPassword" ) );
-        cy.get( '#wp-submit' ).click();
-        cy.visit('/?cb_item=basictest-noadmin&cb-location=32');
-    } );
+  function getTestDate() {
+    let dt = new Date();
+    var test = new Date(dt.getTime()),
+        month = test.getMonth();
+  
+    test.setDate(test.getDate() + 1);
+    if (test.getMonth() !== month ) {
+      dt.setDate(dt.getDate() + 1);
+    }
+
+    return dt.getTime();
+  }
+
+  beforeEach( function() {
+    cy.clock(getTestDate());
+    cy.visit( '/wp-login.php' );
+    cy.wait( 1000 );
+    cy.get( '#user_login' ).type( Cypress.env( "wpSubscriber" ) );
+    cy.get( '#user_pass' ).type( Cypress.env( "wpPassword" ) );
+    cy.get( '#wp-submit' ).click();
+    cy.visit('/?cb_item=basictest-noadmin&cb-location=32');
+  } );
+
   it('grays out button when no date is selected', () => {
     cy.screenshot('booking-form_no-date-selected');
     cy.get('#booking-form > [type="submit"]').should('be.disabled');
@@ -15,15 +30,7 @@ describe('test booking process', () => {
         //we have to do different assertions when we are in the last day of the current month
         //this is because the next date available in the calendar is the first day of the next month
         //and is therefore not in the same div as the current day
-        let lastDayOfMonth = false;
-        //TODO: I don't know how to check this, so expect the test to fail on the last day of the month
-        if (lastDayOfMonth){
-            //make assertions with first day of next month
-            cy.get('.container__months').get('.month-item').next().first('.day-item').click();
-        }
-        else {
-            cy.get('.is-today').click();
-        }
+        cy.get('.is-today').click();
         //just click next directly adjacent day
         cy.get('.is-start-date').next('.day-item').click();
         cy.get('#booking-form > [type="submit"]').should('not.be.disabled');
@@ -31,19 +38,8 @@ describe('test booking process', () => {
         cy.screenshot('booking-form_dates-selected');
     } )
 
-    it ('can abort booking', () => {
-        //we have to do different assertions when we are in the last day of the current month
-        //this is because the next date available in the calendar is the first day of the next month
-        //and is therefore not in the same div as the current day
-        let lastDayOfMonth = false;
-        //TODO: I don't know how to check this, so expect the test to fail on the last day of the month
-        if (lastDayOfMonth){
-            //make assertions with first day of next month
-            cy.get('.container__months').get('.month-item').next().first('.day-item').click();
-        }
-        else {
-            cy.get('.is-today').click();
-        }
+    it ('can abort booking process early', function () {
+        cy.get('.is-today').click();
         //just click next directly adjacent day
         cy.get('.is-start-date').next('.day-item').click();
         cy.get('#booking-form > [type="submit"]').click();
@@ -53,20 +49,8 @@ describe('test booking process', () => {
         cy.get('.cb-notice').contains('Booking canceled');
         cy.screenshot('booking-form_booking-delete-unconfirmed');
     } )
-    it('can book an item', () => {
-        //ATTENTION, after this tests, the above tests won't work anymore, because the item is booked and therefore the current day is not available anymore
-        //we have to do different assertions when we are in the last day of the current month
-        //this is because the next date available in the calendar is the first day of the next month
-        //and is therefore not in the same div as the current day
-        let lastDayOfMonth = false;
-        //TODO: I don't know how to check this, so expect the test to fail on the last day of the month
-        if (lastDayOfMonth){
-            //make assertions with first day of next month
-            cy.get('.container__months').get('.month-item').next().first('.day-item').click();
-        }
-        else {
-            cy.get('.is-today').click();
-        }
+    it('can book an item', function()  {
+        cy.get('.is-today').click();
         //just click next directly adjacent day
         cy.get('.is-start-date').next('.day-item').click();
         cy.get('#booking-form > [type="submit"]').click();
