bf21aef6420f381dd166f986711d2fd6920fdd68
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2023-11-07 18:34:28 -0500

    removed unnecessary todo

--- src/Service/Holiday.php
+++ src/Service/Holiday.php
@@ -7,7 +7,6 @@ class Holiday {
 	/**
 	 * Will render the fields in the timeframe settings where the user can define the holidays to get for the different states and years.
 	 * The actual holidays will be loaded through feiertagejs.
-	 * TODO: These do not necessarily need to be CMB2 fields but can just be custom HTML fields. All the necessary information is pulled through javascript already.
 	 * @param $field
 	 * @param $value
 	 * @param $object_id
