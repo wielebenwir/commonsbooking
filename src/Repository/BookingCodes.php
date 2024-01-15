abb9e994b59f602b812cd08c02abf0944c0a0c17
Nils Larsen <nils.larsen@posteo.de>
2024-01-15 17:09:03 +0100

    harmonization of $advanceGenerationDays: change getCode() such that a value of 0 will generate a code for the reference day $date, just like getCodes() and generate(). This slight behavior change is just to make code consistent, clearer and easier to understand. It was not a bug, but a small inconsistency, and it is not covered by the tests. Since getCode() was never called with $advanceGenerationDays=0, the change will not be noticed in real world.

--- src/Repository/BookingCodes.php
+++ src/Repository/BookingCodes.php
@@ -167,7 +167,7 @@ class BookingCodes {
 	 * @param int $itemId - ID of item attached to timeframe
 	 * @param int $locationId - ID of location attached to timeframe (DEPRECATED)
 	 * @param string $date - Date in format Y-m-d
-	 * @param int $advanceGenerationDays - Open-ended timeframes: If 1, generates code(s) until $date. If >1 generate additional codes after $date.
+	 * @param int $advanceGenerationDays - Open-ended timeframes: If 0, generates code(s) until $date. If >0 generate additional codes after $date.
 	 *
 	 * @return BookingCode|null
 	 * @throws BookingCodeException
@@ -186,8 +186,9 @@ class BookingCodes {
 					$begin = $timeframe->getUTCStartDateDateTime();
 					$endDate = new \DateTime($date);
 					$endDate->modify('+' . $advanceGenerationDays . ' days');
-					/* NOTE: no code will be generated for $endDate, DatePeriod's endDate is only included in
-					iteration if endDate's time is > 00:00:00. Could be set with $endDate->setTime(0,0,1); */
+					// set $endDate's time > 00:00:00 so it will included in DatePeriod iteration and a code will be
+					// generated for $endDate
+					$endDate->setTime(0, 0, 1);
 					$interval = DateInterval::createFromDateString( '1 day' );
 					$period = new DatePeriod( $begin, $interval, $endDate );
 					static::generatePeriod($timeframe,$period);
