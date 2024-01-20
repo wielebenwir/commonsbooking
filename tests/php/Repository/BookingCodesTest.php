dc8b14249c576f745970a0756f0d5471e8688221
Nils Larsen <nils.larsen@posteo.de>
2024-01-20 19:00:19 +0100

    add unit tests for eternal booking codes

--- tests/php/Repository/BookingCodesTest.php
+++ tests/php/Repository/BookingCodesTest.php
@@ -121,6 +121,89 @@ class BookingCodesTest extends CustomPostTypeTest
 		$this->assertEquals( $code->getCode(), $stillSameCode->getCode() );
 	}
 
+	public function testIfCodesAreEternal() {
+		ClockMock::freeze( new \DateTime( self::CURRENT_DATE ) );
+
+		// create timeframe with parameters like $timeframeWithoutEndDate
+		$timeframe_1 = new Timeframe($this->createTimeframe(
+			$this->locationId,
+			$this->itemId,
+			strtotime( '-1 day', strtotime( self::CURRENT_DATE ) ),
+			null,
+		));
+
+		// generate some codes
+		BookingCodes::generate( $timeframe_1, self::ADVANCE_GENERATION_DAYS );
+
+		// get code for $this->itemId today
+		$todaysCode = BookingCodes::getCode( $timeframe_1,
+			$this->itemId,
+			$this->locationId,
+			date('Y-m-d', strtotime( self::CURRENT_DATE )),
+			self::ADVANCE_GENERATION_DAYS);
+
+		$this->assertNotEmpty($todaysCode->getCode());
+
+		// Check if codes are persistant/eternal:
+		// check that codes are persistant, ie when a code is once generated for a certain item and date, it should never change again
+		$countBefore = $this->countBookingCodes();
+		$this->assertGreaterThan(0, $countBefore);
+
+		// add some booking codes (like a WP Admin would do on Commonbookings admin pages)
+		$oldBookingCodes = Settings::getOption( 'commonsbooking_options_bookingcodes', 'bookingcodes' );
+		$updateSuccessful = Settings::updateOption('commonsbooking_options_bookingcodes','bookingcodes',"$oldBookingCodes,NewCode");
+		$this->assertTrue($updateSuccessful);
+
+		// repeat generate() with same parameters as above ...
+		BookingCodes::generate( $timeframe_1, self::ADVANCE_GENERATION_DAYS );
+
+		// ... it should NOT lead to any new codes, because they are already existing. Check by counting:
+		$countAfter = $this->countBookingCodes();
+		$this->assertEquals($countBefore, $countAfter);
+
+		// ... and check by comparing today's code
+		$todaysCodeAfter = BookingCodes::getCode( $timeframe_1,
+			$this->itemId,
+			$this->locationId,
+			date('Y-m-d', strtotime( self::CURRENT_DATE )),
+			self::ADVANCE_GENERATION_DAYS);
+
+		$this->assertEquals($todaysCode->getCode(), $todaysCodeAfter->getCode());
+
+		// now delete timeframe and create another timeframe with same parameters (especially same item and overlapping in time)
+		wp_delete_post( $timeframe_1->ID, true );
+
+		$timeframe_2 = new Timeframe($this->createTimeframe(
+			$this->locationId,
+			$this->itemId,
+			strtotime( '-1 day', strtotime( self::CURRENT_DATE ) ),
+			null,
+		));
+
+		// repeat generate() with same parameters as above ...
+		BookingCodes::generate( $timeframe_2, self::ADVANCE_GENERATION_DAYS );
+
+		// ... and check by comparing today's code
+		$todaysCodeTimeframe2 = BookingCodes::getCode( $timeframe_2,
+			$this->itemId,
+			$this->locationId,
+			date('Y-m-d', strtotime( self::CURRENT_DATE )),
+			self::ADVANCE_GENERATION_DAYS);
+
+		// even if it is another timeframe, the today's code for the same item must still be the same
+		$this->assertEquals($todaysCode->getCode(), $todaysCodeTimeframe2->getCode());
+
+	}
+
+	private function countBookingCodes() {
+		global $wpdb;
+		$table_name = $wpdb->prefix . BookingCodes::$tablename;
+
+		$sql = "SELECT COUNT(*) FROM $table_name";
+
+		return $wpdb->get_var($sql);
+	}
+
 	public function testGetCodes() {
 		ClockMock::freeze( new \DateTime( self::CURRENT_DATE ) );
 		//make sure that we get no codes before generation
