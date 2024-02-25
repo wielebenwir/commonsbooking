1ae4f2d2fd4465170b9368315ae9c613e9eaedfb
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2024-02-25 16:43:28 +0100

    grunt after merge

--- assets/admin/js/admin.js
+++ assets/admin/js/admin.js
@@ -1,6 +1,85 @@
 (function($) {
     "use strict";
     $(function() {
+        let fullDayCheckbox = $("#full-day");
+        let startTimeInput = $("#repetition-start_time");
+        let endTimeInput = $("#repetition-end_time");
+        let preserveManualCode = false;
+        fullDayCheckbox.on("change", function(event) {
+            if (fullDayCheckbox.is(":checked")) {
+                startTimeInput.val("00:00");
+                endTimeInput.val("23:59");
+                startTimeInput.hide();
+                endTimeInput.hide();
+            } else {
+                startTimeInput.show();
+                endTimeInput.show();
+            }
+        });
+        fullDayCheckbox.trigger("change");
+        let itemInput = $("#item-id");
+        let locationInput = $("#location-id");
+        let startDateInput = $("#repetition-start_date");
+        let bookingCodeInput = $("#_cb_bookingcode");
+        itemInput.on("change", function(event) {
+            let data = {
+                itemID: itemInput.val()
+            };
+            const fetchLocation = data => {
+                $.post(cb_ajax_get_bookable_location.ajax_url, {
+                    _ajax_nonce: cb_ajax_get_bookable_location.nonce,
+                    action: "cb_get_bookable_location",
+                    data: data
+                }, function(data) {
+                    if (data.success) {
+                        locationInput.val(data.locationID);
+                        fullDayCheckbox.prop("checked", data.fullDay);
+                        fullDayCheckbox.trigger("change");
+                    }
+                }).then(() => {
+                    fetchBookingCode();
+                });
+            };
+            fetchLocation(data);
+        });
+        itemInput.trigger("change");
+        const fetchBookingCode = () => {
+            if (!fullDayCheckbox.is(":checked")) {
+                return;
+            }
+            let data = {
+                itemID: itemInput.val(),
+                locationID: locationInput.val(),
+                startDate: startDateInput.val()
+            };
+            $.post(cb_ajax_get_booking_code.ajax_url, {
+                _ajax_nonce: cb_ajax_get_booking_code.nonce,
+                action: "cb_get_booking_code",
+                data: data
+            }, function(data) {
+                if (data.success) {
+                    bookingCodeInput.val(data.bookingCode);
+                    preserveManualCode = false;
+                } else if (!preserveManualCode) {
+                    bookingCodeInput.val("");
+                }
+            });
+        };
+        bookingCodeInput.on("keyup", function(event) {
+            preserveManualCode = true;
+        });
+        startDateInput.on("change", function(event) {
+            fetchBookingCode();
+        });
+        fullDayCheckbox.on("change", function(event) {
+            fetchBookingCode();
+        });
+    });
+})(jQuery);
+
+(function($) {
+    "use strict";
+    $(function() {
         const groupName = "rules_group";
         const groupID = "cmb-group-rules_group-";
         const ruleSelectorID = "rule-type";
