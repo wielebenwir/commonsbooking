wielebenwir/commonsbooking

commit 8d98c00317e219dc0185adab8ec1e9f88ebba949

Feature/holiday timeframe multiselect (#1296)
* Frontend of multi-select timeframes
* multiselect logic
* migrated over more from experiment branch
* added tests for multi-tfs
* fixed unit tests
* fixed multi-select js (now exclusively for holidays)
* added hooks to update multi selection on dynamic selections
* added deprecation warning
* removed unused function
* fixed confusing wording
* added methods  & tests to get multiple items & locations from one tf
* more tests for timeframes with multi-assigned items / locations
* modified export to support multiply applied timeframes
* fixed caching issues for tfs with multiple assignments
* marked what needs to be done before #507
* moved upgrade function & added test (failing)
* added selection mode to tf creation in tests
* Adds also a test for the location taxonomy query
* add hook to trigger update on term deletion
* make sure, that multi-ids are always deleted for all non-holiday tfs
* added check for getLocations() with just one location
* use getter function for postID instead of magic property

---------

Co-authored-by: markus-mw <markus@mach-websites.de>
Co-authored-by: Chris <datengraben@gmx.de>
Co-authored-by: Chris <105302830+datengraben@users.noreply.github.com>

src/View/TimeframeExport.php
@@ -71,13 +71,14 @@ class TimeframeExport {
 
 		$headline = false;
 
+		$timeframeDataRows = self::getTimeframeData( $timeframes );
+
 		/** @var \CommonsBooking\Model\Timeframe $timeframePost */
-		foreach ( $timeframes as $timeframePost ) {
-			$timeframeData = self::getTimeframeData( $timeframePost );
+		foreach ( $timeframeDataRows as $timeframeDataRow ) {
 
 			if ( ! $headline ) {
 				$headline    = true;
-				$headColumns = array_keys( $timeframeData );
+				$headColumns = array_keys( $timeframeDataRow );
 
 				// Iterate through in put fields
 				foreach ( $inputFields as $type => $fields ) {
@@ -93,7 +94,7 @@ class TimeframeExport {
 			}
 
 			// output the column values
-			$valueColumns = array_values( $timeframeData );
+			$valueColumns = array_values( $timeframeDataRow );
 
 			// Get values for user defined input fields.
 			foreach ( $inputFields as $type => $fields ) {
@@ -219,77 +220,89 @@ class TimeframeExport {
 	}
 
 	/**
-	 * Prepares timeframe data array.
+	 * Takes an array of timeframes and returns an array of timeframe data for the table export
 	 *
-	 * @param \CommonsBooking\Model\Timeframe $timeframePost
+	 * @param \CommonsBooking\Model\Timeframe[] $timeframePosts
 	 *
 	 * @return array
 	 */
-	protected static function getTimeframeData( \CommonsBooking\Model\Timeframe $timeframePost ): array {
-		$timeframeData = self::getRelevantTimeframeFields( $timeframePost );
+	public static function getTimeframeData( array $timeframePosts ): array {
 
-		// Timeframe typ
-		$timeframeTypeId       = $timeframePost->getFieldValue( 'type' );
-		$timeframetypes        = \CommonsBooking\Wordpress\CustomPostType\Timeframe::getTypes();
-		$timeframeData['type'] = array_key_exists( $timeframeTypeId, $timeframetypes ) ?
-			$timeframetypes[ $timeframeTypeId ] : __( 'Unknown', 'commonsbooking' );
+		$timeframeDataRows = [];
+		foreach ( $timeframePosts as $timeframePost ) {
+			$timeframeData = self::getRelevantTimeframeFields( $timeframePost );
+			// Timeframe typ
+			$timeframeTypeId       = $timeframePost->getFieldValue( 'type' );
+			$timeframeTypes        = \CommonsBooking\Wordpress\CustomPostType\Timeframe::getTypes();
+			$timeframeData['type'] = array_key_exists( $timeframeTypeId, $timeframeTypes ) ?
+				$timeframeTypes[ $timeframeTypeId ] : __( 'Unknown', 'commonsbooking' );
 
-		if ( $timeframeTypeId == \CommonsBooking\Wordpress\CustomPostType\Timeframe::BOOKING_ID ) {
-			$booking = new \CommonsBooking\Model\Booking( $timeframePost->ID );
+			if ( $timeframeTypeId == \CommonsBooking\Wordpress\CustomPostType\Timeframe::BOOKING_ID ) {
+				$booking = new \CommonsBooking\Model\Booking( $timeframePost->ID );
+			}
+
+			// Repetition option
+			$repetitions                      = \CommonsBooking\Wordpress\CustomPostType\Timeframe::getTimeFrameRepetitions();
+			$repetitionId                     = $timeframePost->getFieldValue( \CommonsBooking\Model\Timeframe::META_REPETITION );
+			$timeframeData[ \CommonsBooking\Model\Timeframe::META_REPETITION ] = array_key_exists( $repetitionId, $repetitions ) ?
+				$repetitions[ $repetitionId ] : __( 'Unknown', 'commonsbooking' );
+
+			// Grid option
+			$gridOptions           = \CommonsBooking\Wordpress\CustomPostType\Timeframe::getGridOptions();
+			$gridOptionId          = $timeframePost->getGrid();
+			$timeframeData["grid"] = array_key_exists( $gridOptionId, $gridOptions ) ?
+				$gridOptions[ $gridOptionId ] : __( 'Unknown', 'commonsbooking' );
+
+
+			// get corresponding item title(s)
+			$items = $timeframePost->getItems();
+			if ( $items != null ) {
+				$items_title = array_map( function ( $item ) {
+					return $item->post_title;
+				}, $items );
+			} else {
+				$items_title = __( 'Unknown', 'commonsbooking' );
+			}
+
+			// get corresponding location title(s)
+			$locations = $timeframePost->getLocations();
+			if ( $locations != null ) {
+				$locations_title = array_map( function ( $location ) {
+					return $location->post_title;
+				}, $locations );
+			} else {
+				$locations_title = __( 'Unknown', 'commonsbooking' );
+			}
+
+			// populate simple meta fields
+			$timeframeData[ \CommonsBooking\Model\Timeframe::META_MAX_DAYS ]    = $timeframePost->getFieldValue( \CommonsBooking\Model\Timeframe::META_MAX_DAYS );
+			$timeframeData["full-day"]                                          = $timeframePost->getFieldValue( "full-day" );
+			$timeframeData[ \CommonsBooking\Model\Timeframe::REPETITION_START ] =
+				$timeframePost->getStartDate() ?
+					date( 'c', $timeframePost->getStartDate() ) : '';
+			$timeframeData[ \CommonsBooking\Model\Timeframe::REPETITION_END ]   =
+				$timeframePost->getEndDate() ?
+					date( 'c', $timeframePost->getEndDate() ) : '';
+			$timeframeData["start-time"]                                        = $timeframePost->getStartTime();
+			$timeframeData["end-time"]                                          = $timeframePost->getEndTime();
+			$timeframeData["pickup"]                                            = isset( $booking ) ? $booking->pickupDatetime() : "";
+			$timeframeData["return"]                                            = isset( $booking ) ? $booking->returnDatetime() : "";
+			$timeframeData["booking-code"]                                      = $timeframePost->getFieldValue( "_cb_bookingcode" );
+			$timeframeData["user-firstname"]                                    = $timeframePost->getUserData()->first_name;
+			$timeframeData["user-lastname"]                                     = $timeframePost->getUserData()->last_name;
+			$timeframeData["user-login"]                                        = $timeframePost->getUserData()->user_login;
+			$timeframeData["comment"]                                           = $timeframePost->getFieldValue( 'comment' );
+
+			foreach ( $locations_title as $location_title ) {
+				foreach ( $items_title as $item_title ) {
+					$timeframeData["location-post_title"] = $location_title;
+					$timeframeData["item-post_title"]     = $item_title;
+					//every item / location combination is a new row
+					$timeframeDataRows[] 				= $timeframeData;
+				}
+			}
 		}
-
-		// Repetition option
-		$repetitions                           = \CommonsBooking\Wordpress\CustomPostType\Timeframe::getTimeFrameRepetitions();
-		$repetitionId                          = $timeframePost->getFieldValue( META_REPETITION );
-		$timeframeData[ META_REPETITION ] = array_key_exists( $repetitionId, $repetitions ) ?
-			$repetitions[ $repetitionId ] : __( 'Unknown', 'commonsbooking' );
-
-		// Grid option
-		$gridOptions           = \CommonsBooking\Wordpress\CustomPostType\Timeframe::getGridOptions();
-		$gridOptionId          = $timeframePost->getGrid();
-		$timeframeData["grid"] = array_key_exists( $gridOptionId, $gridOptions ) ?
-			$gridOptions[ $gridOptionId ] : __( 'Unknown', 'commonsbooking' );
-
-		// get corresponding item title
-		$item = $timeframePost->getItem();
-		if ($item != null){
-			$item_title = $item->post_title;
-		}
-		else {
-			$item_title = __( 'Unknown', 'commonsbooking' );
-		}
-
-		// get corresponding location title
-		$location = $timeframePost->getLocation();
-		if ($location != null){
-			$location_title = $location->post_title;
-		}
-		else {
-			$location_title = __( 'Unknown', 'commonsbooking' );
-		}
-
-		// populate simple meta fields
-		$timeframeData[ \CommonsBooking\Model\Timeframe::META_MAX_DAYS ]  = $timeframePost->getFieldValue( \CommonsBooking\Model\Timeframe::META_MAX_DAYS );
-		$timeframeData["full-day"]            = $timeframePost->getFieldValue( "full-day" );
-		$timeframeData[\CommonsBooking\Model\Timeframe::REPETITION_START] =
-			$timeframePost->getStartDate() ?
-				date( 'c', $timeframePost->getStartDate() ) : '';
-		$timeframeData[\CommonsBooking\Model\Timeframe::REPETITION_END] =
-			$timeframePost->getEndDate() ?
-				date( 'c', $timeframePost->getEndDate() ) : '';
-		$timeframeData["start-time"]          = $timeframePost->getStartTime();
-		$timeframeData["end-time"]            = $timeframePost->getEndTime();
-		$timeframeData["pickup"]              = isset( $booking ) ? $booking->pickupDatetime() : "";
-		$timeframeData["return"]              = isset( $booking ) ? $booking->returnDatetime() : "";
-		$timeframeData["booking-code"]        = $timeframePost->getFieldValue( "_cb_bookingcode" );
-		$timeframeData["location-post_title"] = $location_title;
-		$timeframeData["item-post_title"]     = $item_title;
-		$timeframeData["user-firstname"]      = $timeframePost->getUserData()->first_name;
-		$timeframeData["user-lastname"]       = $timeframePost->getUserData()->last_name;
-		$timeframeData["user-login"]          = $timeframePost->getUserData()->user_login;
-		$timeframeData["comment"]             = $timeframePost->getFieldValue('comment');
-
-		return $timeframeData;
+		return $timeframeDataRows;
 	}
 
 	/**

tests/php/View/TimeframeExportTest.php
@@ -1,0 +1,45 @@
+<?php
+
+namespace CommonsBooking\Tests\View;
+
+use CommonsBooking\Model\Booking;
+use CommonsBooking\Model\Timeframe;
+use CommonsBooking\Tests\Wordpress\CustomPostTypeTest;
+use CommonsBooking\View\TimeframeExport;
+
+class TimeframeExportTest extends CustomPostTypeTest {
+
+	public function testGetTimeframeData() {
+		$timeframeOneItemAndLocation = new Timeframe( $this->createBookableTimeFrameIncludingCurrentDay() );
+		$dataArray = TimeframeExport::getTimeframeData( [ $timeframeOneItemAndLocation ] );
+		$this->assertEquals( 1, count( $dataArray ) );
+
+		$secondItem = $this->createItem( 'second-item' );
+		$timeframeTwoItemsOneLocation = new Timeframe (
+			$this->createTimeframe(
+				$this->locationId,
+				[ $this->itemId, $secondItem ],
+				strtotime( self::CURRENT_DATE ),
+				strtotime( '+1 week', strtotime( self::CURRENT_DATE ) )
+			)
+		);
+		$dataArray = TimeframeExport::getTimeframeData( [ $timeframeTwoItemsOneLocation ] );
+		$this->assertEquals( 2, count( $dataArray ) );
+
+		$secondLocation = $this->createLocation( 'second-location' );
+		$timeframeTwoItemsTwoLocations = new Timeframe (
+			$this->createTimeframe(
+				[ $this->locationId, $secondLocation ],
+				[ $this->itemId, $secondItem ],
+				strtotime( self::CURRENT_DATE ),
+				strtotime( '+1 week', strtotime( self::CURRENT_DATE ) )
+			)
+		);
+		$dataArray = TimeframeExport::getTimeframeData( [ $timeframeTwoItemsTwoLocations ] );
+		$this->assertEquals( 4, count( $dataArray ) );
+
+		$booking = new Booking($this->createConfirmedBookingStartingToday());
+		$dataArray = TimeframeExport::getTimeframeData( [ $booking ] );
+		$this->assertEquals( 1, count( $dataArray ) );
+	}
+}

assets/admin/js/admin.js
@@ -229,6 +229,12 @@
         const REPETITION_WEEKLY = "w";
         const REPETITION_MONTHLY = "m";
         const REPETITION_YEARLY = "y";
+        const SELECTION_MANUAL = 0;
+        const SELECTION_CATEGORY = 1;
+        const SELECTION_ALL = 2;
+        const timeframeRepetitionInput = $("#timeframe-repetition");
+        const locationSelectionInput = $("#location-select");
+        const itemSelectionInput = $("#item-select");
         if (timeframeForm.length) {
             const timeframeRepetitionInput = $("#timeframe-repetition");
             const typeInput = $("#type");
@@ -251,6 +257,12 @@
             const linkSendEntireTimeframeCodes = $("#email-booking-codes-list-all");
             const linkSendCurrentMonth = $("#email-booking-codes-list-current");
             const linkSendNextMonth = $("#email-booking-codes-list-next");
+            const singleLocationSelection = $(".cmb2-id-location-id");
+            const multiLocationSelection = $(".cmb2-id-location-id-list");
+            const singleItemSelection = $(".cmb2-id-item-id");
+            const multiItemSelection = $(".cmb2-id-item-id-list");
+            const categoryLocationSelection = $(".cmb2-id-location-category-ids");
+            const categoryItemSelection = $(".cmb2-id-item-category-ids");
             const holidayField = $(".cmb2-id--cmb2-holiday");
             const holidayInput = $("#timeframe_manual_date");
             const manualDatePicker = $("#cmb2_multiselect_datepicker");
@@ -280,6 +292,28 @@
                     $(this).prop("checked", false);
                 });
             };
+            const migrateSingleSelection = () => {
+                if (typeInput.val() != HOLIDAYS_ID) {
+                    return;
+                }
+                const singleItemSelectionOption = singleItemSelection.find("option:selected");
+                if (singleItemSelectionOption.prop("value")) {
+                    const multiItemSelectionOption = multiItemSelection.find(`input[value=${singleItemSelectionOption.prop("value")}]`);
+                    if (multiItemSelectionOption) {
+                        multiItemSelectionOption.prop("checked", true);
+                    }
+                    singleItemSelectionOption.prop("selected", false);
+                }
+                const singleLocationSelectionOption = singleLocationSelection.find("option:selected");
+                if (singleLocationSelectionOption.prop("value")) {
+                    const multiLocationSelectionOption = multiLocationSelection.find(`input[value=${singleLocationSelectionOption.prop("value")}]`);
+                    if (multiLocationSelectionOption) {
+                        multiLocationSelectionOption.prop("checked", true);
+                    }
+                    singleLocationSelectionOption.prop("selected", false);
+                }
+            };
+            migrateSingleSelection();
             const handleTypeSelection = function() {
                 const selectedType = $("option:selected", typeInput).val();
                 const selectedRepetition = $("option:selected", timeframeRepetitionInput).val();
@@ -296,10 +330,70 @@
                         holidayField.hide();
                     }
                 }
+                if (selectedType == HOLIDAYS_ID) {
+                    itemSelectionInput.show();
+                    locationSelectionInput.show();
+                    migrateSingleSelection();
+                } else {
+                    itemSelectionInput.hide();
+                    locationSelectionInput.hide();
+                }
             };
             handleTypeSelection();
             typeInput.change(function() {
                 handleTypeSelection();
+                handleItemSelection();
+                handleLocationSelection();
+            });
+            const handleLocationSelection = function() {
+                const selectedType = $("option:selected", typeInput).val();
+                if (selectedType == HOLIDAYS_ID) {
+                    singleLocationSelection.hide();
+                    const selectedOption = $("option:selected", locationSelectionInput).val();
+                    if (selectedOption == SELECTION_MANUAL) {
+                        multiLocationSelection.show();
+                        categoryLocationSelection.hide();
+                    } else if (selectedOption == SELECTION_CATEGORY) {
+                        categoryLocationSelection.show();
+                        multiLocationSelection.hide();
+                    } else if (selectedOption == SELECTION_ALL) {
+                        multiLocationSelection.hide();
+                        categoryLocationSelection.hide();
+                    }
+                } else {
+                    singleLocationSelection.show();
+                    multiLocationSelection.hide();
+                    categoryLocationSelection.hide();
+                }
+            };
+            handleLocationSelection();
+            locationSelectionInput.change(function() {
+                handleLocationSelection();
+            });
+            const handleItemSelection = function() {
+                const selectedType = $("option:selected", typeInput).val();
+                if (selectedType == HOLIDAYS_ID) {
+                    singleItemSelection.hide();
+                    const selectedOption = $("option:selected", itemSelectionInput).val();
+                    if (selectedOption == SELECTION_MANUAL) {
+                        multiItemSelection.show();
+                        categoryItemSelection.hide();
+                    } else if (selectedOption == SELECTION_CATEGORY) {
+                        categoryItemSelection.show();
+                        multiItemSelection.hide();
+                    } else if (selectedOption == SELECTION_ALL) {
+                        multiItemSelection.hide();
+                        categoryItemSelection.hide();
+                    }
+                } else {
+                    singleItemSelection.show();
+                    multiItemSelection.hide();
+                    categoryItemSelection.hide();
+                }
+            };
+            handleItemSelection();
+            itemSelectionInput.change(function() {
+                handleItemSelection();
             });
             const handleFullDaySelection = function() {
                 const selectedRep = $("option:selected", timeframeRepetitionInput).val();

commit 159efdcfc1e24495cbb43ded26b4bf5229e758dc

TimeframeExport: Fixes #1532
Adds the fix and additional tests to validate the fix.

The commit 8d98c00317e219dc0185adab8ec1e9f88ebba949 introduced this bug.
It had the purpose to allow timesframes (for holidays) to be "mutli-selected" with more than one location or item.
This fixes the export, but won't include custom-fields of location- and item-posts, related to holiday-timeframes in the
export file.

src/View/TimeframeExport.php
@@ -73,7 +73,6 @@ class TimeframeExport {
 
 		$timeframeDataRows = self::getTimeframeData( $timeframes );
 
-		/** @var \CommonsBooking\Model\Timeframe $timeframePost */
 		foreach ( $timeframeDataRows as $timeframeDataRow ) {
 
 			if ( ! $headline ) {
@@ -96,11 +95,15 @@ class TimeframeExport {
 			// output the column values
 			$valueColumns = array_values( $timeframeDataRow );
 
+			//TODO #507
+			/** @var \CommonsBooking\Model\Timeframe $timeframeDataPost */
+			$timeframeDataPost = new \CommonsBooking\Model\Timeframe( $timeframeDataRow['ID'] );
+
 			// Get values for user defined input fields.
 			foreach ( $inputFields as $type => $fields ) {
 				// Location fields
 				if ( $type == 'location' ) {
-					$location = $timeframePost->getLocation();
+					$location = $timeframeDataPost->getLocation();
 					foreach ( $fields as $field ) {
 						$valueColumns[] = $location->getFieldValue( $field );
 					}
@@ -108,7 +111,7 @@ class TimeframeExport {
 
 				// Item fields
 				if ( $type == 'item' ) {
-					$item = $timeframePost->getItem();
+					$item = $timeframeDataPost->getItem();
 					foreach ( $fields as $field ) {
 						$valueColumns[] = $item->getFieldValue( $field );
 					}
@@ -116,7 +119,7 @@ class TimeframeExport {
 
 				// User fields
 				if ( $type == 'user' ) {
-					$user = $timeframePost->getUserData();
+					$user = $timeframeDataPost->getUserData();
 					foreach ( $fields as $field ) {
 						$valueColumns[] = $user->get( $field );
 					}
@@ -220,7 +223,7 @@ class TimeframeExport {
 	}
 
 	/**
-	 * Takes an array of timeframes and returns an array of timeframe data for the table export
+	 * Takes an array of timeframe posts and returns an array of timeframe assoc array data for the table export
 	 *
 	 * @param \CommonsBooking\Model\Timeframe[] $timeframePosts
 	 *

tests/php/View/TimeframeExportTest.php
@@ -4,10 +4,15 @@ namespace CommonsBooking\Tests\View;
 
 use CommonsBooking\Model\Booking;
 use CommonsBooking\Model\Timeframe;
+use CommonsBooking\Settings\Settings;
 use CommonsBooking\Tests\Wordpress\CustomPostTypeTest;
 use CommonsBooking\View\TimeframeExport;
+use SlopeIt\ClockMock\ClockMock;
 
 class TimeframeExportTest extends CustomPostTypeTest {
+
+	protected $fileUnderTest;
+	protected $directoryUnderTest;
 
 	public function testGetTimeframeData() {
 		$timeframeOneItemAndLocation = new Timeframe( $this->createBookableTimeFrameIncludingCurrentDay() );
@@ -41,5 +46,101 @@ class TimeframeExportTest extends CustomPostTypeTest {
 		$booking = new Booking($this->createConfirmedBookingStartingToday());
 		$dataArray = TimeframeExport::getTimeframeData( [ $booking ] );
 		$this->assertEquals( 1, count( $dataArray ) );
+	}
+
+	/**
+	 * @ignore
+	 */
+	public function testExportCsv_testsFileCreation() {
+
+		// TODO think about changing the signature of exportCSV or getExportData to include startDate and endDate, then it is less coupled to the actual usage atm
+		ClockMock::freeze( new \DateTime( self::CURRENT_DATE ) );
+		Settings::updateOption( 'commonsbooking_options_export', 'export-timerange', 14 );
+
+		$timeframeOneItemAndLocation = new Timeframe( $this->createBookableTimeFrameIncludingCurrentDay() );
+
+		TimeframeExport::exportCsv( $this->directoryUnderTest );
+
+		$fileName = scandir( $this->directoryUnderTest )[2];
+		$this->fileUnderTest = $this->directoryUnderTest . $fileName;
+
+		// Check basic properties
+		$this->assertTrue( file_exists( $this->fileUnderTest ) );
+		$this->assertGreaterThan( 0, filesize( $this->fileUnderTest ) );
+		ClockMock::reset();
+
+		// Parse csv contents
+		$i = 0;
+		$header = null;
+		$firstLine = null;
+		$file = fopen($this->fileUnderTest, 'r');
+		while (($line = fgetcsv($file)) !== FALSE) {
+			if ( $i == 0 ) {
+				$header = $line[0];
+			}
+			if ( $i == 1 ) {
+				$firstLine = $line[0];
+			}
+			$i += 1;
+		}
+		fclose($file);
+
+		$this->assertNotNull( $header );
+		$firstColumn = explode( ";", $header)[0];
+
+		$this->assertEquals( "ID", $firstColumn );
+		$this->assertNotNull( $firstLine );
+	}
+
+	public function testGetExportData() {
+		ClockMock::freeze( new \DateTime( self::CURRENT_DATE ) );
+		$timeframeOneItemAndLocation = new Timeframe( $this->createBookableTimeFrameIncludingCurrentDay() );
+		$dataArray = TimeframeExport::getTimeframeData( [ $timeframeOneItemAndLocation ] );
+		$this->assertEquals( 1, count( $dataArray ) );
+
+		$secondItem = $this->createItem( 'second-item' );
+		$timeframeTwoItemsOneLocation = new Timeframe (
+			$this->createTimeframe(
+				$this->locationId,
+				[ $this->itemId, $secondItem ],
+				strtotime( self::CURRENT_DATE ),
+				strtotime( '+1 week', strtotime( self::CURRENT_DATE ) )
+			)
+		);
+		$dataArray = TimeframeExport::getTimeframeData( [ $timeframeTwoItemsOneLocation ] );
+		$this->assertEquals( 2, count( $dataArray ) );
+
+		Settings::updateOption( 'commonsbooking_options_export', 'export-timerange', '14' );
+
+		$result = TimeFrameExport::getExportData( true );
+
+		$this->assertEquals( 2, count( $result ));
+		ClockMock::reset();
+	}
+
+	public function tearDown():void {
+		parent::tearDown();
+
+		if ($this->fileUnderTest)
+			wp_delete_file( $this->fileUnderTest );
+
+		rmdir( $this->directoryUnderTest );
+	}
+
+	public function setUp(): void {
+		parent::setUp();
+
+		$this->directoryUnderTest = '/tmp/commonsbooking-timeframe-export-directory/';
+
+		// Deletes directory if existent
+		if ( file_exists( $this->directoryUnderTest ) ) {
+			foreach( scandir( $this->directoryUnderTest ) as $fileInDir ) {
+				wp_delete_file( $this->directoryUnderTest . $fileInDir );
+			}
+			rmdir( $this->directoryUnderTest );
+		}
+
+		// Creates new
+		mkdir( $this->directoryUnderTest );
 	}
 }