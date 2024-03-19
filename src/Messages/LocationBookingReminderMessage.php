457bde3cacbab36c48c735068cbfdf828c46b94e
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2024-03-19 15:34:27 +0100

    formatting

--- src/Messages/LocationBookingReminderMessage.php
+++ src/Messages/LocationBookingReminderMessage.php
@@ -5,7 +5,6 @@ namespace CommonsBooking\Messages;
 use CommonsBooking\CB\CB;
 use CommonsBooking\Model\MessageRecipient;
 use CommonsBooking\Repository\Booking;
-use CommonsBooking\Service\Scheduler;
 use CommonsBooking\Settings\Settings;
 use CommonsBooking\Wordpress\CustomPostType\Location;
 
@@ -16,28 +15,28 @@ use CommonsBooking\Wordpress\CustomPostType\Location;
  */
 class LocationBookingReminderMessage extends Message {
 
-  /**
+	/**
 	 * @var array|string[]
 	 */
-	protected $validActions = [ "booking-start-location-reminder", "booking-end-location-reminder"  ];
+	protected $validActions = [ "booking-start-location-reminder", "booking-end-location-reminder" ];
 
 	/**
 	 * Prepares reminder message
 	 */
 	public function prepareMessage() {
 		/** @var \CommonsBooking\Model\Booking $booking */
-		$booking = Booking::getPostById( $this->getPostId() );
+		$booking      = Booking::getPostById( $this->getPostId() );
 		$booking_user = get_userdata( $this->getPost()->post_author );
 
 		// get location email adresses to send them bcc copies
-		$location = get_post($booking->getMeta('location-id'));
-		$location_emails_option = CB::get( Location::$postType, COMMONSBOOKING_METABOX_PREFIX . 'location_email', $location ) ; /*  email addresses, comma-seperated  */
-		if(empty($location_emails_option)) {
+		$location               = get_post( $booking->getMeta( 'location-id' ) );
+		$location_emails_option = CB::get( Location::$postType, COMMONSBOOKING_METABOX_PREFIX . 'location_email', $location ); /*  email addresses, comma-seperated  */
+		if ( empty( $location_emails_option ) ) {
 			return;
 		}
 
-		$location_emails_option = str_replace(' ','',$location_emails_option); 
-		$location_emails = explode(',', $location_emails_option);
+		$location_emails_option = str_replace( ' ', '', $location_emails_option );
+		$location_emails        = explode( ',', $location_emails_option );
 
 		// get templates from Admin Options
 		$template_body    = Settings::getOption( 'commonsbooking_options_reminder',
@@ -52,7 +51,7 @@ class LocationBookingReminderMessage extends Message {
 			sanitize_email( Settings::getOption( 'commonsbooking_options_templates', 'emailheaders_from-email' ) )
 		);
 
-		if(!is_array($location_emails)) {
+		if ( ! is_array( $location_emails ) ) {
 			return;
 		}
 
@@ -80,5 +79,5 @@ class LocationBookingReminderMessage extends Message {
 	 */
 	public function sendMessage() {
 		$this->SendNotificationMail();
-	 }
+	}
 }
