371dc3ea52b287a0ed10e847f8ba3dc0150b6b9d
christian-wlw <github@farbschein.de>
2024-04-11 17:57:31 +0200

    changed table columns to datetime

--- src/Repository/TimeframeRelations.php
+++ src/Repository/TimeframeRelations.php
@@ -18,8 +18,8 @@ class TimeframeRelations
 			timeframe bigint(20) unsigned NOT NULL,
 			location bigint(20) unsigned NOT NULL,
 			item bigint(20) unsigned NOT NULL,
-			startTS bigint(20) unsigned NOT NULL,
-			endTS bigint(20) unsigned NOT NULL,
+			StartDateTime DATETIME NOT NULL,
+			EndDateTime DATETEIME NOT NULL,
 			type tinyint(1) unsigned NOT NULL,
 			PRIMARY KEY (timeframe, location, item, type)
 		) $charsetCollate;";
@@ -36,13 +36,13 @@ class TimeframeRelations
 		$tableName = $wpdb->prefix . self::$tableName;
 		$locationIDs = $timeframe->getLocationIDs();
 		$itemIDs = $timeframe->getItemIDs();
-		$startTS = $timeframe->getStartDate();
-		$endTS = $timeframe->getEndDate();
+		$StartDateTime = date('Y-m-d H:i:s', $timeframe->getStartDate());
+		$EndDateTime = date('Y-m-d H:i:s', $timeframe->getEndDate());
 		$type = $timeframe->getType();
 
 		foreach ($locationIDs as $locationID) {
 			foreach ($itemIDs as $itemID) {
-				$sql = $wpdb->prepare("INSERT INTO $tableName (timeframe, location, item, startTS, endTS, type) VALUES (%d, %d, %d, %d, %d, %d)", $timeframe->ID, $locationID, $itemID, $startTS, $endTS, $type);
+				$sql = $wpdb->prepare("INSERT INTO $tableName (timeframe, location, item, StartDateTime, EndDateTime, type) VALUES (%d, %d, %d, %d, %d, %d)", $timeframe->ID, $locationID, $itemID, $StartDateTime, $EndDateTime, $type);
 				$wpdb->query($sql);
 			}
 		}
@@ -74,7 +74,7 @@ class TimeframeRelations
 		$locationString = implode(',', $locations);
 		$itemString = implode(',', $items);
 		$typeString = implode(',', $types);
-		$sql = $wpdb->prepare("SELECT DISTINCT timeframe FROM $tableName WHERE location IN (%s) AND item IN (%s) AND startTS <= %d AND endTS >= %d AND type IN (%s)", $locationString, $itemString, $dateTS, $dateTS, $typeString);
+		$sql = $wpdb->prepare("SELECT DISTINCT timeframe FROM $tableName WHERE location IN (%s) AND item IN (%s) AND StartDateTime <= %d AND EndDateTime >= %d AND type IN (%s)", $locationString, $itemString, $dateTS, $dateTS, $typeString);
 		$result = $wpdb->get_results($sql);
 		$ids = [];
 		foreach ($result as $row) {
