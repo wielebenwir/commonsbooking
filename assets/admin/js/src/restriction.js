144b3112af65589586bb62c80129e67b575f616c
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2024-05-08 13:24:59 +0000

    more specific selector

--- assets/admin/js/src/restriction.js
+++ assets/admin/js/src/restriction.js
@@ -1,7 +1,7 @@
 (function ($) {
     'use strict';
     $(function () {
-        const form = $('input[name=post_type][value=cb_restriction]').parent('form').find('cmb2-wrap');
+        const form = $('input[name=post_type][value=cb_restriction]').parent('form').find('#cb_restriction-custom-fields');
 
         // disable send button on change of form
         form.find('input, select, textarea').on('keyup change paste', function () {
