034f033747a9c37c8762521155a41aa6da2a362b
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2024-05-08 13:10:23 +0000

    fix #1586 - only apply for cmb2

--- assets/admin/js/src/restriction.js
+++ assets/admin/js/src/restriction.js
@@ -1,7 +1,7 @@
 (function ($) {
     'use strict';
     $(function () {
-        const form = $('input[name=post_type][value=cb_restriction]').parent('form');
+        const form = $('input[name=post_type][value=cb_restriction]').parent('form').find('cmb2-wrap');
 
         // disable send button on change of form
         form.find('input, select, textarea').on('keyup change paste', function () {
