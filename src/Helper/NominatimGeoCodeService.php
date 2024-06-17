218954c9fd981e695132111c187340feab23a85c
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2024-06-17 18:36:27 +0200

    use identifying user-agent for nominatim: Prevents requests being blocked

--- src/Helper/NominatimGeoCodeService.php
+++ src/Helper/NominatimGeoCodeService.php
@@ -19,9 +19,11 @@ class NominatimGeoCodeService implements GeoCodeService {
 	 * @param $addressString
 	 *
 	 * @return ?Location
+	 * @throws \Exception
 	 */
 	public function getAddressData( $addressString ): ?Location {
-		$defaultUserAgent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0';
+		
+		$defaultUserAgent = 'CommonsBooking v.' . COMMONSBOOKING_VERSION . " Contact: mail@commonsbooking.org";
 
 		$client = new Client(
 			null,
