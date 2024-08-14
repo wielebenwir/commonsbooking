5c3b37c6bff22436fa8a3ec859acba671c45ae24
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2024-08-14 19:17:07 +0200

    formatting

--- tests/php/Service/MassOperationsTest.php
+++ tests/php/Service/MassOperationsTest.php
@@ -8,29 +8,27 @@ use CommonsBooking\Service\MassOperations;
 use CommonsBooking\Tests\Wordpress\CustomPostTypeTest;
 use SlopeIt\ClockMock\ClockMock;
 
-class MassOperationsTest extends CustomPostTypeTest
-{
+class MassOperationsTest extends CustomPostTypeTest {
 	private Timeframe $testTimeframe;
 
-    public function testMigrateOrphaned()
-    {
-		ClockMock::freeze(new \DateTime(self::CURRENT_DATE));
+	public function testMigrateOrphaned() {
+		ClockMock::freeze( new \DateTime( self::CURRENT_DATE ) );
 		//create an orphaned booking
-	    $toOrphan = new Booking( $this->createConfirmedBookingStartingToday() );
-		$newLocation = $this->createLocation("New Location",'publish');
-		update_post_meta($this->testTimeframe->ID,'location-id',$newLocation);
+		$toOrphan    = new Booking( $this->createConfirmedBookingStartingToday() );
+		$newLocation = $this->createLocation( "New Location", 'publish' );
+		update_post_meta( $this->testTimeframe->ID, 'location-id', $newLocation );
 
-		$this->assertTrue($toOrphan->isOrphaned());
+		$this->assertTrue( $toOrphan->isOrphaned() );
 		$orphans = \CommonsBooking\Repository\Booking::getOrphaned();
-		$this->assertCount(1,$orphans);
-		MassOperations::migrateOrphaned([$toOrphan->ID]);
-		$this->assertFalse($toOrphan->isOrphaned());
-		$this->assertEquals($newLocation,get_post_meta($toOrphan->ID,'location-id',true));
+		$this->assertCount( 1, $orphans );
+		MassOperations::migrateOrphaned( [ $toOrphan->ID ] );
+		$this->assertFalse( $toOrphan->isOrphaned() );
+		$this->assertEquals( $newLocation, get_post_meta( $toOrphan->ID, 'location-id', true ) );
 
 		//empty array given
-        $result = MassOperations::migrateOrphaned([]);
-		$this->assertFalse($result['success']);
-    }
+		$result = MassOperations::migrateOrphaned( [] );
+		$this->assertFalse( $result['success'] );
+	}
 
 	protected function setUp(): void {
 		parent::setUp();
