c970cf8009b8d7f3e72af1a4d520a4fffff39b94
Chris <105302830+datengraben@users.noreply.github.com>
2024-11-26 14:43:56 +0100

    Update Calendar.php

--- src/View/Calendar.php
+++ src/View/Calendar.php
@@ -89,7 +89,8 @@ class Calendar {
 
 		$print  = '<div class="cb-table-scroll">';
 		$print .= "<table class='cb-items-table tablesorter'><colgroup><col><col>" . $colStr . '</colgroup><thead>';
-		$accessible_table_header_tag = empty($desc) ? "td" : "th" // Use td-tag when no table header description is given, to match semantics of header cells
+		// Use td-tag when no table header description is given, to match semantics of header cells
+		$accessible_table_header_tag = empty($desc) ? "td" : "th"; 
 		$print .= "<tr><$accessible_table_header_tag colspan='2' class='sortless'>" . $desc . "</$accessible_table_header_tag>";
 
 		// Render months
