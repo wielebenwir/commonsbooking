f0136e8592c1644d65ac46266f95f2c8270e68dc
Chris <105302830+datengraben@users.noreply.github.com>
2025-12-17 15:07:55 +0100

    try with higher timeout

--- tests/cypress/cypress.config.js
+++ tests/cypress/cypress.config.js
@@ -12,6 +12,7 @@ module.exports = defineConfig({
   },
   e2e: {
     baseUrl: 'http://localhost:1001/',
+    pageLoadTimeout: 120000,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
