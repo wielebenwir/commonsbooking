12cd74c0b67ccf403c104e3b3b703de9568e042e
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2026-03-03 12:26:10 +0100

    fix finding now correctly identified by phpstan

--- src/Repository/Timeframe.php
+++ src/Repository/Timeframe.php
@@ -103,7 +103,7 @@ class Timeframe extends PostRepository {
 	 *
 	 * @return int[]|\WP_Post[]|\CommonsBooking\Model\Timeframe[]|\CommonsBooking\Model\Booking[]
 	 * @throws Exception
-	 * @throws \CommonsBooking\Psr\Cache\InvalidArgumentException|\CommonsBooking\Psr\Cache\CacheException @phpstan-ignore throws.notThrowable
+	 * @throws \CommonsBooking\Psr\Cache\InvalidArgumentException|\CommonsBooking\Psr\Cache\CacheException
 	 */
 	public static function get(
 		array $locations = [],
