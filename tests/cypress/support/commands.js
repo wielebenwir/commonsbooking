03c5172f0662f3da8bf5ee471117c962520cf159
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2026-03-31 19:42:16 +0200

    test for #2124 admin cannot cancel other bookings

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -16,18 +16,27 @@ Cypress.Commands.add('loginAs', (userType = 'admin') => {
   const username = userType;
   const password = Cypress.env('wpPassword');
 
-  // Just do simple login - no retry, but with better error messages
-  cy.visit('/wp-login.php', { 
+  cy.visit('/wp-login.php', {
     timeout: 120000, // 2 minutes for CI
-    failOnStatusCode: false 
+    failOnStatusCode: false
   });
 
   cy.wait(2000) //2 seconds so login form is completely present
-  
+
   cy.get('#user_login', { timeout: 30000 }).type(username);
   cy.get('#user_pass').type(password);
   cy.get('#wp-submit').click();
 
   // Final check
   cy.url({ timeout: 30000 }).should('include', '/wp-admin/');
+});
+
+Cypress.Commands.add('logout', () => {
+    cy.visit('/wp-admin');
+    cy.get('body').then($body => {
+        if ($body.find('#wpadminbar').length !== 0) {
+            cy.get('#wp-admin-bar-my-account').invoke('addClass', 'hover');
+            cy.get('#wp-admin-bar-logout > a').click();
+        }
+    });
 });
