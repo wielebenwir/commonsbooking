a0b833a4dab944311097b0ef21f7c58e61b20347
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2026-04-15 10:56:59 +0200

    add getLocation to Item model to retrieve currently stationed location

--- tests/php/Model/ItemTest.php
+++ tests/php/Model/ItemTest.php
@@ -6,6 +6,7 @@ use CommonsBooking\Model\Item;
 use CommonsBooking\Model\Restriction;
 use CommonsBooking\Model\Timeframe;
 use CommonsBooking\Tests\Wordpress\CustomPostTypeTest;
+use SlopeIt\ClockMock\ClockMock;
 
 class ItemTest extends CustomPostTypeTest {
 
@@ -54,6 +55,65 @@ class ItemTest extends CustomPostTypeTest {
 		$this->assertEquals($restrictionArray, $this->itemModel->getRestrictions());
 	}
 	*/
+
+	public function testGetLocation() {
+		$dt = new \DateTime( self::CURRENT_DATE );
+		ClockMock::freeze( $dt );
+
+		// just one item that is currently bookable
+		$this->assertEquals( $this->locationId, $this->itemModel->getLocation()->ID );
+
+		// in two weeks, the timeframe is expired, so the item will not be at the location anymore
+		$dt->modify( '+2 weeks' );
+		ClockMock::freeze( $dt );
+		$this->assertNull( $this->itemModel->getLocation() );
+
+		// item that is in location A monday-thursday and in location B friday-sunday. Position should change depending on the day of the week
+		$locationA       = $this->createLocation( 'location a' );
+		$locationB       = $this->createLocation( 'location b' );
+		$movingItem      = $this->createItem( 'location changing item' );
+		$movingItemModel = new Item( $movingItem );
+		$tf1             = new Timeframe(
+			$this->createTimeframe(
+				$locationA,
+				$movingItem,
+				strtotime( '-1 day', strtotime( \CommonsBooking\Tests\Wordpress\CustomPostTypeTest::CURRENT_DATE ) ),
+				strtotime( '+14 days', strtotime( \CommonsBooking\Tests\Wordpress\CustomPostTypeTest::CURRENT_DATE ) ),
+				\CommonsBooking\Wordpress\CustomPostType\Timeframe::BOOKABLE_ID,
+				'',
+				'w',
+				0,
+				'8:00 AM',
+				'12:00 PM',
+				'publish',
+				[ '1','2','3','4' ]
+			)
+		);
+		$tf2             = new Timeframe(
+			$this->createTimeframe(
+				$locationB,
+				$movingItem,
+				strtotime( '-1 day', strtotime( \CommonsBooking\Tests\Wordpress\CustomPostTypeTest::CURRENT_DATE ) ),
+				strtotime( '+14 days', strtotime( \CommonsBooking\Tests\Wordpress\CustomPostTypeTest::CURRENT_DATE ) ),
+				\CommonsBooking\Wordpress\CustomPostType\Timeframe::BOOKABLE_ID,
+				'',
+				'w',
+				0,
+				'8:00 AM',
+				'12:00 PM',
+				'publish',
+				[ '5','6','7' ]
+			)
+		);
+		$this->assertFalse( $tf1->overlaps( $tf2 ) );
+		$dt = new \DateTime( self::CURRENT_DATE );
+		$dt->modify( 'monday' );
+		ClockMock::freeze( $dt );
+		$this->assertEquals( $locationA, $movingItemModel->getLocation()->ID );
+		$dt->modify( 'friday' );
+		ClockMock::freeze( $dt );
+		$this->assertEquals( $locationB, $movingItemModel->getLocation()->ID );
+	}
 
 	protected function setUp(): void {
 		parent::setUp();
