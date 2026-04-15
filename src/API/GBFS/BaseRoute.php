6b168a905d44991134346497ebd97bc1cc671c3f
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2026-04-15 11:00:51 +0200

    GBFS: made item that base-route iterates over customizable

--- src/API/GBFS/BaseRoute.php
+++ src/API/GBFS/BaseRoute.php
@@ -4,6 +4,7 @@
 namespace CommonsBooking\API\GBFS;
 
 use CommonsBooking\Repository\Location;
+use CommonsBooking\Repository\PostRepository;
 use Exception;
 use stdClass;
 use WP_REST_Request;
@@ -11,10 +12,14 @@ use WP_REST_Response;
 
 /**
  * Base class which implements retrieval of basic data attributes of
- * GBFS spec.
+ * GBFS spec. Derive from this class, when you want to have a route that iterates over CustomPosts.
  *
- * Note: When deriving from this class, implement \WP_REST_Controller::prepare_item_for_response,
- *       which is called in \BaseRoute::getItemData
+ * Note: When deriving from this class
+ *      - implement \WP_REST_Controller::prepare_item_for_response,
+ *        which is called in \BaseRoute::getItemData
+ *      - implement $rest_base
+ *      - implement $schemaUrl
+ *      - (if necessary) overwrite getRepository
  */
 class BaseRoute extends \CommonsBooking\API\BaseRoute {
 
@@ -26,12 +31,12 @@ class BaseRoute extends \CommonsBooking\API\BaseRoute {
 	 * @return WP_REST_Response
 	 */
 	public function get_items( $request ): WP_REST_Response {
-		$response                 = new stdClass();
-		$response->data           = new stdClass();
-		$response->data->stations = $this->getItemData( $request );
-		$response->last_updated   = date( 'c' ); // ISO-8601 timestamp
-		$response->ttl            = 60;
-		$response->version        = '3.1-RC2';
+		$response                                = new stdClass();
+		$response->data                          = new stdClass();
+		$response->data->{static::getListName()} = $this->getItemData( $request );
+		$response->last_updated                  = date( 'c' ); // ISO-8601 timestamp
+		$response->ttl                           = 60;
+		$response->version                       = '3.1-RC2';
 
 		if ( WP_DEBUG ) {
 			$this->validateData( $response );
@@ -48,12 +53,12 @@ class BaseRoute extends \CommonsBooking\API\BaseRoute {
 	 * @return array
 	 */
 	public function getItemData( $request ): array {
-		$data      = [];
-		$locations = Location::get();
+		$data  = [];
+		$items = static::getRepository()::get();
 
-		foreach ( $locations as $location ) {
+		foreach ( $items as $item ) {
 			try {
-				$itemdata = $this->prepare_item_for_response( $location, $request );
+				$itemdata = $this->prepare_item_for_response( $item, $request );
 				$data[]   = $itemdata->data;
 			} catch ( Exception $exception ) {
 				if ( WP_DEBUG ) {
@@ -63,5 +68,24 @@ class BaseRoute extends \CommonsBooking\API\BaseRoute {
 		}
 
 		return $data;
+	}
+
+	/**
+	 * Overwrite this, if you don't iterate over stations but need the resulting items in a list with a different name
+	 *
+	 * @return string
+	 */
+	protected static function getListName(): string {
+		return 'stations';
+	}
+
+	/**
+	 * The post type that the route will iterate over.
+	 * By default, these are all the locations.
+	 *
+	 * @return PostRepository
+	 */
+	protected static function getRepository(): PostRepository {
+		return new Location();
 	}
 }
