8a5f43015885e8aca429bf16dd948dd54a29a296
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2026-04-27 14:45:30 +0200

    add unit tests for GBFS routes station_information and system_information

--- tests/php/API/GBFS/SystemInformationRouteTest.php
+++ tests/php/API/GBFS/SystemInformationRouteTest.php
@@ -10,4 +10,24 @@ use CommonsBooking\Tests\API\CB_REST_Route_UnitTestCase;
 class SystemInformationRouteTest extends CB_REST_Route_UnitTestCase {
 
 	protected $ENDPOINT = '/commonsbooking/v1/system_information.json';
+
+	public function testRoute() {
+		$request  = new \WP_REST_Request( 'GET', $this->ENDPOINT );
+		$response = rest_do_request( $request );
+
+		$data = $response->get_data()->data;
+		$this->assertNotEmpty( $data->name );
+		$this->assertNotEmpty( $data->system_id );
+
+		$header = $response->get_data(); // the top level info that is always present
+
+		$this->assertNotFalse( \DateTime::createFromFormat( \DateTime::ATOM, $header->last_updated ) );
+		$this->assertIsInt( $header->ttl );
+		$this->assertIsString( $header->version );
+		parent::testRoute();
+	}
+
+	public function setUp(): void {
+		parent::setUp();
+	}
 }
