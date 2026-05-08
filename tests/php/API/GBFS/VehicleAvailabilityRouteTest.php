f380aad453ed6427f583297e68652bbd611e3d2b
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2026-05-08 09:41:47 +0200

    tests: add test for GBFS availability with offset calculation

--- tests/php/API/GBFS/VehicleAvailabilityRouteTest.php
+++ tests/php/API/GBFS/VehicleAvailabilityRouteTest.php
@@ -4,6 +4,7 @@ namespace CommonsBooking\Tests\API\GBFS;
 
 use CommonsBooking\Model\Timeframe;
 use CommonsBooking\Tests\API\CB_REST_Route_UnitTestCase;
+use CommonsBooking\Tests\Wordpress\CustomPostTypeTest;
 use SlopeIt\ClockMock\ClockMock;
 
 class VehicleAvailabilityRouteTest extends CB_REST_Route_UnitTestCase {
@@ -22,6 +23,60 @@ class VehicleAvailabilityRouteTest extends CB_REST_Route_UnitTestCase {
 		$data = $response->get_data()->data;
 		$this->assertNotEmpty( $data->vehicles );
 		$this->assertCount( 1, $data->vehicles );
+		$availabilities = $data->vehicles[0]->availabilities;
+		$this->assertCount( 1, $availabilities );
+
+		$startDT = new \DateTime( $availabilities[0]->from );
+		$today   = new \DateTime( self::CURRENT_DATE );
+
+		$this->assertEqualsWithDelta( $today->getTimestamp(), $startDT->getTimestamp(), 1.0 );
+	}
+
+	public function testAvailabilityWithOffset() {
+		ClockMock::freeze( new \DateTime( self::CURRENT_DATE ) );
+
+		// delete other timeframe so it doesn't mess with our tests
+		wp_delete_post( $this->timeframe, true );
+		$otherLocationId = $this->createLocation( 'Other Location', );
+		$otherItemId     = $this->createItem( 'Other Item', );
+
+		$timeframeID = $this->createTimeframe(
+			$otherLocationId,
+			$otherItemId,
+			strtotime( '-1 day', strtotime( self::CURRENT_DATE ) ),
+			strtotime( '+10 days', strtotime( self::CURRENT_DATE ) ),
+			\CommonsBooking\Wordpress\CustomPostType\Timeframe::BOOKABLE_ID,
+			'on',
+			'd',
+			0,
+			'8:00 AM',
+			'12:00 PM',
+			'publish',
+			[],
+			'',
+			CustomPostTypeTest::USER_ID,
+			3,
+			30,
+			2
+		);
+
+		// with offset → not available right now
+		$request  = new \WP_REST_Request( 'GET', $this->ENDPOINT );
+		$response = rest_do_request( $request );
+		$data     = $response->get_data()->data;
+
+		$availabilities = $data->vehicles[0]->availabilities;
+
+		$startDT = new \DateTime( $availabilities[0]->from );
+		$today   = new \DateTime( self::CURRENT_DATE );
+
+		$this->assertNotEqualsWithDelta( $today->getTimestamp(), $startDT->getTimestamp(), 1.0 );
+
+		// remove offset → now available today
+		update_post_meta( $timeframeID, \CommonsBooking\Model\Timeframe::META_BOOKING_START_DAY_OFFSET, 0 );
+
+		$response       = rest_do_request( $request );
+		$data           = $response->get_data()->data;
 		$availabilities = $data->vehicles[0]->availabilities;
 		$this->assertCount( 1, $availabilities );
 
