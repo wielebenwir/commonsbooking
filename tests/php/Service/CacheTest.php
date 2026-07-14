c8e7506727eac10111f0a01265d60dcd33e52e45
Dirk Drutschmann <dirkdrutschmann@gmail.com>
2026-07-14 18:17:41 +0200

    Test cache backtrace depth limit

--- tests/php/Service/CacheTest.php
+++ tests/php/Service/CacheTest.php
@@ -7,6 +7,48 @@ use PHPUnit\Framework\TestCase;
 
 class CacheTest extends TestCase {
 
+	public function testGetCacheIdLimitsBacktraceDepth() {
+		if ( ! function_exists( 'uopz_set_return' ) ) {
+			$this->markTestSkipped( 'The uopz extension is required to inspect the debug_backtrace call.' );
+		}
+
+		$backtraceOptions = null;
+		$backtraceLimit   = null;
+		$backtrace        = [
+			[],
+			[],
+			[
+				'class'    => self::class,
+				'function' => __FUNCTION__,
+				'args'     => [],
+			],
+		];
+
+		uopz_set_return(
+			'debug_backtrace',
+			function ( $options = DEBUG_BACKTRACE_PROVIDE_OBJECT, $limit = 0 ) use (
+				&$backtraceOptions,
+				&$backtraceLimit,
+				$backtrace
+			) {
+				$backtraceOptions = $options;
+				$backtraceLimit   = $limit;
+
+				return $backtrace;
+			},
+			true
+		);
+
+		try {
+			Plugin::getCacheId();
+		} finally {
+			uopz_unset_return( 'debug_backtrace' );
+		}
+
+		$this->assertSame( 0, $backtraceOptions );
+		$this->assertSame( 3, $backtraceLimit );
+	}
+
 	public function testGetAdapters() {
 		// check, if the adapters exist
 		$adapters = Plugin::getAdapters();
