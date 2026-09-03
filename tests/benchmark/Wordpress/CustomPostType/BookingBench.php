af8ba24e5df2b7b85f7eaa8f96dda1df9cf43a86
Hans Morbach <6433480+hansmorb@users.noreply.github.com>
2026-09-03 11:28:33 +0200

    add benchmarks for booking rules

--- tests/benchmark/Wordpress/CustomPostType/BookingBench.php
+++ tests/benchmark/Wordpress/CustomPostType/BookingBench.php
@@ -3,6 +3,7 @@
 namespace CommonsBooking\Tests\Benchmark\Wordpress\CustomPostType;
 
 use CommonsBooking\Repository\BookingCodes;
+use CommonsBooking\Settings\Settings;
 use CommonsBooking\Tests\Benchmark\BenchmarkCase;
 use CommonsBooking\Wordpress\CustomPostType\Booking;
 
@@ -12,6 +13,8 @@ class BookingBench extends BenchmarkCase {
 	private int $benchmarkLocationId;
 	private int $repetitionStart;
 	private int $repetitionEnd;
+	private ?int $bookingRulesUserId = null;
+	private $previousBookingRules;
 
 	/**
 	 * @BeforeMethods({"setUp"})
@@ -20,6 +23,21 @@ class BookingBench extends BenchmarkCase {
 	 * @Revs(3)
 	 */
 	public function benchBookingLifecycle(): void {
+		$this->runBookingLifecycle();
+	}
+
+	/**
+	 * @BeforeMethods({"setUp", "enableBookingRule"})
+	 * @AfterMethods({"tearDown"})
+	 * @Iterations(6)
+	 * @Revs(3)
+	 * @ParamProviders({"provideBookingRules"})
+	 */
+	public function benchBookingLifecycleWithBookingRule( array $params ): void {
+		$this->runBookingLifecycle();
+	}
+
+	private function runBookingLifecycle(): void {
 		$bookingId          = $this->handleBookingRequest( 'unconfirmed' );
 		$this->bookingIds[] = $bookingId;
 		$postName           = get_post( $bookingId )->post_name;
@@ -44,6 +62,81 @@ class BookingBench extends BenchmarkCase {
 		);
 		update_post_meta( $timeframeId, \CommonsBooking\Model\Timeframe::META_SHOW_BOOKING_CODES, '' );
 		update_post_meta( $timeframeId, \CommonsBooking\Model\Timeframe::META_CREATE_BOOKING_CODES, '' );
+	}
+
+	public function enableBookingRule( array $params ): void {
+		$this->previousBookingRules = Settings::getOption( 'commonsbooking_options_restrictions', 'rules_group' );
+		Settings::updateOption(
+			'commonsbooking_options_restrictions',
+			'rules_group',
+			[ $params['rule'] ]
+		);
+
+		$this->bookingRulesUserId = wp_create_user(
+			'benchmark-booking-rules-' . wp_generate_uuid4(),
+			wp_generate_password()
+		);
+		wp_set_current_user( $this->bookingRulesUserId );
+	}
+
+	public function provideBookingRules(): iterable {
+		$appliesToAll = [ 'rule-applies-all' => 'on' ];
+
+		yield 'no simultaneous booking' => [
+			'rule' => $appliesToAll + [ 'rule-type' => 'noSimultaneousBooking' ],
+		];
+		yield 'prohibit chain booking' => [
+			'rule' => $appliesToAll + [ 'rule-type' => 'prohibitChainBooking' ],
+		];
+		yield 'max booking days per week' => [
+			'rule' => $appliesToAll + [
+				'rule-type'         => 'maxBookingDaysPerWeek',
+				'rule-param1'       => 100,
+				'rule-select-param' => 1,
+			],
+		];
+		yield 'max booking days per month' => [
+			'rule' => $appliesToAll + [
+				'rule-type'         => 'maxBookingDaysPerMonth',
+				'rule-param1'       => 100,
+				'rule-select-param' => 1,
+			],
+		];
+		yield 'max booking days' => [
+			'rule' => $appliesToAll + [
+				'rule-type'   => 'maxBookingDays',
+				'rule-param1' => 100,
+				'rule-param2' => 365,
+			],
+		];
+		yield 'max bookings per week' => [
+			'rule' => $appliesToAll + [
+				'rule-type'         => 'maxBookingsWeek',
+				'rule-param1'       => 100,
+				'rule-select-param' => 1,
+			],
+		];
+		yield 'max bookings per month' => [
+			'rule' => $appliesToAll + [
+				'rule-type'         => 'maxBookingsMonth',
+				'rule-param1'       => 100,
+				'rule-select-param' => 1,
+			],
+		];
+	}
+
+	public function tearDown(): void {
+		if ( $this->bookingRulesUserId !== null ) {
+			Settings::updateOption(
+				'commonsbooking_options_restrictions',
+				'rules_group',
+				$this->previousBookingRules
+			);
+			wp_delete_user( $this->bookingRulesUserId );
+			$this->bookingRulesUserId = null;
+		}
+
+		parent::tearDown();
 	}
 
 	private function handleBookingRequest(
